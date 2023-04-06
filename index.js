b8d897575adf60efd51b49c21ecd446507f650e5
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-04-07 00:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const submissions = require('./submissions');
 const app = express();
 
 app.use(bodyParser.json());
+const questions = [
+  {
+    title: 'Two states',
+    description: 'Given an array, return the maximum of the array?',
+    testCases: [
+      {
+        input: '[1,2,3,4,5]',
+        output: '5',
+      },
+    ],
+  },
+];
+const submissions = [];
 
 app.post('/signup', function (req, res) {
   // implementation for user signup
