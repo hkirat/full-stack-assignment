63d8b44225dfaa8f7ce84e0ca41f500fd2842aa3
Vishal <vishalr.21.beis@acharya.ac.in>
2023-04-07 00:58:09 +0530

    Completed Hardtodos

--- index.js
+++ index.js
@@ -15,17 +15,16 @@ const QUESTIONS = [{
     }]
 }];
 
+const SUBMISSION = []
 
-const SUBMISSION = [
-
-]
+const tokens = []
 
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-  const {username, password} = req.body
-  USERS.push({username, password})
+  const {username, password, usertype} = req.body
+  USERS.push({username, password, usertype})
 
 
   // return back 200 status code to the client
@@ -75,13 +74,34 @@ app.get('/questions', function(req, res) {
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
-    const solution = req.body
+    const {token} = req.body
+    if(tokens.includes(token))
+    {
+      const solution = req.body
     SUBMISSION.push(solution)
+    res.status(200).send('ok')}
+    else {
+      res.status(401).send('Unauthorized')
+    }
   });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
+app.post("/addquestions", function(req, res) {
+    const {token, username} = req.body
+    if(tokens.includes(token)){
+      const user = USERS.find((user) => user.username === username)
+      if(user.usertype === 'admin'){
+        const question = req.body
+        QUESTIONS.push(question)
+        res.status(200).send('ok')
+      }
+      else {
+        res.status(401).send('Unauthorized')
+      }
+    }
+})
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
