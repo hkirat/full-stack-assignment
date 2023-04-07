330c27a0ad1db1690ace3429d15ccf24835594ae
Prakhar Tomar <prakhart4@gmail.com>
2023-04-07 19:46:53 +0530

    all done

--- index.js
+++ index.js
@@ -1,73 +1,186 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const validator = require("validator");
+const app = express();
+const port = 3001;
 
-const USERS = [];
+const USERS = []; //{ email: "example@domain.com", password: "myNameIsKaran", role: "admin" }
 
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+        output: "5",
+      },
+    ],
+  },
+];
 
+const SUBMISSION = [];
 
-const SUBMISSION = [
+const AccessToken_Random_string =
+  "jasfbasbhjdacuoasdnbvaufisfifvipuafafvqfn9u43fr9qvb49f8rnwfreq";
 
-]
+// middleware
+app.use(express.json());
 
-app.post('/signup', function(req, res) {
+//User signup
+app.post("/signup", (req, res) => {
   // Add logic to decode body
   // body should have email and password
-
+  const { email, password, role } = req.body;
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  switch (true) {
+    case !email || !password || !role:
+      return res.status(400).json({ message: "All fields not provided" });
+    case !validator.isEmail(email):
+      return res.status(400).json({ message: "Not a valid email" });
+    case USERS.some((v) => v.email === email):
+      return res.status(400).json({ message: "Email already registered" });
+    case !validator.isStrongPassword(password):
+      return res.status(400).json({ message: "Not a valid password" });
+    case !["admin", "user"].includes(role):
+      return res
+        .status(400)
+        .json({ message: 'Role must be either "user" or "admin".' });
+  }
 
+  try {
+    USERS.push({ email, password, role });
+    res.status(200).json({
+      user: { email, role },
+      accessToken: AccessToken_Random_string + ">" + email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
+//user Login
+app.post("/login", function (req, res) {
   // Add logic to decode body
   // body should have email and password
+  const { email, password } = req.body;
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  switch (true) {
+    case !email || !password:
+      return res.status(400).json({ message: "All fields not provided" });
+    case !validator.isEmail(email):
+      return res.status(400).json({ message: "Not a valid email" });
 
+    // Check if the user with the given email exists in the USERS array
+    // Also ensure that the password is the same
+    // If the password is the same, return back 200 status code to the client
+    // Also send back a token (any random string will do for now)
+    // If the password is not the same, return back 401 status code to the client
+    case !USERS.some((v) => v.email === email):
+      return res.status(401).json({ message: "User not found" });
+    case USERS.find((v) => v.email === email)?.password !== password:
+      return res.status(401).json({ message: "Incorrect password" });
+  }
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  res
+    .status(200)
+    .json({ accessToken: AccessToken_Random_string + ">" + email });
+});
 
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
+app.get("/questions", function (req, res) {
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+  if (!isAuth(req))
+    return res.status(401).json({ message: "User not authenticated" });
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  res.status(200).json(QUESTIONS);
 });
 
+app.post("/questions", function (req, res) {
+  // leaving as hard todos
+  // Create a route that lets an admin add a new problem
+  // ensure that only admins can do that.
+  const { title, description, testCases } = req.body;
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  switch (true) {
+    case !isAuth(req):
+      return res.status(401).json({ message: "User not authenticated" });
+    case getUser(req)?.role !== "admin":
+      return res.status(401).json({ message: "Only admins can add questions" });
+    case !title || !description || !testCases:
+      return res.status(400).json({ message: "All fields are not provided" });
+  }
+
+  try {
+    QUESTIONS.push({ title, description, testCases });
+    return res.status(200).json({ message: "Question added" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+app.get("/submissions", function (req, res) {
+  if (!isAuth(req))
+    return res.status(401).json({ message: "User not authenticated" });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  res.status(200).json({
+    submissions: SUBMISSION.filter((v) => v.user === getUser(req)?.email),
+  });
+});
+
+app.post("/submissions", function (req, res) {
+  const { question, submission } = req.body;
+
+  switch (true) {
+    case !isAuth(req):
+      return res.status(401).json({ message: "User not authenticated" });
+    case !question || !submission:
+      return res.status(400).json({ message: "All fields are not provided" });
+    case !QUESTIONS.some((q) => q.title === question):
+      return res.status(400).json({ message: "Invalid Question submitted" });
+  }
+
+  const result = Math.random() > 0.5;
+
+  try {
+    SUBMISSION.push({
+      user: getUser(req)?.email,
+      question,
+      submission,
+      result,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.status(200).json({ result });
+});
+
+//util
+const isAuth = (req) => {
+  const Authorization = req.headers["authorization"];
+
+  if (!Authorization)
+    return res.status(401).json({ message: "Authorization missing" });
+
+  const [type, reqAccessToken, email] = Authorization.split(/\s|>/);
+  console.log({ reqAccessToken });
+
+  return reqAccessToken === AccessToken_Random_string ? true : false;
+};
+
+const getUser = (req) => {
+  const Authorization = req.headers["authorization"];
+
+  if (!Authorization)
+    return res.status(401).json({ message: "Authorization missing" });
+
+  const [type, reqAccessToken, email] = Authorization.split(/\s|>/);
+
+  const currentUser = USERS.find((v) => (v.email = email));
+
+  return reqAccessToken === AccessToken_Random_string ? currentUser : undefined;
+};
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
