54787bdc568db089d7d4e86373b0b3313e3f193e
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-07 21:40:51 +0530

    Implemented the Routes as per the Task assigned.

--- index.js
+++ index.js
@@ -2,72 +2,156 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-const USERS = [];
 
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+app.use(express.json());
 
+const USERS = [
+    {
+        email: "sbharath35@gmail.com",
+        password: "openSource@99"
+    }, {
+        email: "reddy@gmail.com",
+        password: "openSourceIsCool@#12"
+    }];
 
-const SUBMISSION = [
-
-]
-
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+const QUESTIONS = [
+    {
+        title: "Two states",
+        description: "Given an array , return the maximum of the array?",
+        testCases: [{
+            input: "[1,2,3,4,5]", output: "5"
+        }]
+    }];
 
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
+const SUBMISSION = [{
+    userId: "1", questionId: "1", code: "function max(arr) {return Math.max(...arr)}", status: "accepted"
+}]
+
+const ERRORS = {
+    MISSING_DETAILS: {code: 403, message: 'Please provide both email and password.'},
+    WRONG_CREDENTIALS: {code: 403, message: 'Invalid email or password.'},
+    USER_NOT_FOUND: {code: 404, message: 'User not found.'},
+    INVALID_SUBMISSION: {code: 400, message: 'Invalid submission. Please check your details and try again.'},
+    SERVER_ERROR: {code: 500, message: 'Internal server error. Please try again later.'},
+
+};
+
+app.post('/signup', async (req, res) => {
+    try {
+
+        const {email, password} = req.body;
+        const existingUser = USERS.find(user => user.email === email);
+
+        if (existingUser) {
+            return res.status(409).send('User already exists');
+        }
+
+        USERS.push({email, password});
+
+        res.status(200).send('User created successfully!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error');
+    }
 })
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+app.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
 
+        if (!email || !password) {
+            res.status(ERRORS.MISSING_DETAILS.code).json({error: ERRORS.MISSING_DETAILS.message});
+            return;
+        }
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+        const user = USERS.find(u => u.email === email);
 
+        if (!user) {
+            res.status(ERRORS.USER_NOT_FOUND.code).json({error: ERRORS.USER_NOT_FOUND.message});
+            return;
+        }
 
-  res.send('Hello World from route 2!')
+        const passwordMatch = password === user.password;
+
+        if (passwordMatch) {
+            res.status(200).send(`${email} logged in successfully!`);
+            return;
+        }
+
+        res.status(ERRORS.WRONG_CREDENTIALS.code).json({error: ERRORS.WRONG_CREDENTIALS.message});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal server error.'});
+    }
+});
+
+app.get('/questions', (req, res) => {
+    res.json(QUESTIONS);
 })
 
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions", (req, res) => {
+    res.json(SUBMISSION)
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.post('/submissions', (req, res) => {
+    try {
+        const {userId, questionId, code, status} = req.body;
+        const isAccepted = Math.random() < 0.5;
+        if (userId && questionId && code && status) {
+            SUBMISSION.push({
+                userId,
+                questionId,
+                code,
+                status,
+                isAccepted,
+            });
+            res.status(201).json({message: 'Submission added successfully'});
+        } else {
+            res.status(400).json({error: 'Please provide userId, questionId, code, and status.'});
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Internal server error.'});
+    }
 });
 
-// leaving as hard todos
+
+//List of admin email addresses
+const ADMIN_EMAILS = ['admin@example.com', 'superuser@example.com'];
+
 // Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+app.post('/problems', (req, res) => {
+    // Check if user is authenticated
+    const {authorization} = req.headers;
+    if (!authorization) {
+        return res.status(401).send('Authentication required');
+    }
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
+    // Check if user is an admin
+    const admin = isAdmin(authorization);
+    if (!admin) {
+        return res.status(403).send('You do not have permission to perform this action');
+    }
+
+    // Add the new problem to the Array
+    const {title, description, testCases} = req.body;
+    const newProblem = {title, description, testCases};
+    QUESTIONS.push(newProblem);
+
+    // Return a success message to the client
+    res.status(201).send('Problem created successfully');
+});
+
+
+// Helper function to check if a user is an admin
+function isAdmin(userEmail) {
+    return ADMIN_EMAILS.includes(userEmail);
+}
+
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`)
 })
