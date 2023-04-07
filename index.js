7f96c7682d6f4c68ea6bef470de5e8366fa0bfb1
Aditi Bhadoriya <awdhesh1700@gmail.com>
2023-04-07 15:28:12 +0530

    Assignment Completed
    Completed the first assignment of the full stack development series.

--- index.js
+++ index.js
@@ -18,51 +18,91 @@ const SUBMISSION = [
 
 ]
 
+app.use(express.json()); 0
+
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  // Decode the request body
+  const { email, password } = req.body;
 
+  // Check if user already exists
+  const userExists = USERS.find((user) => user.email === email);
+  if (userExists) {
+    return res.status(409).send('User already exists');
+  }
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  // Store new user in USERS array
+  USERS.push({ email, password });
 
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
+  // Return success status code
+  res.sendStatus(200);
+});
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  // Decode the request body
+  const { email, password } = req.body;
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  // Check if user exists and password matches
+  const user = USERS.find((user) => user.email === email && user.password === password);
+  if (!user) {
+    return res.status(401).send('Invalid email or password');
+  }
 
+  // Generate a random token (for now)
+  const token = Math.random().toString(36).substr(2);
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  // Return success status code and token
+  res.status(200).json({ token });
+});
 
-
-  res.send('Hello World from route 2!')
-})
-
+    
 app.get('/questions', function(req, res) {
+  // Return all questions in the QUESTIONS array
+  res.status(200).json(QUESTIONS);
+});
+    
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+ app.get('/submissions', function(req, res) {
+  // Return all submissions for this problem
+  const { problemId } = req.query;
+  const submissions = SUBMISSION.filter(submission => submission.problemId === problemId);
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  res.status(200).json(submissions);
 });
 
+app.post('/submissions', function(req, res) {
+  // Let the user submit a problem, randomly accept or reject the solution
+  const { problemId, solution } = req.body;
+  const isAccepted = Math.random() >= 0.5;
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  // Add new submission to SUBMISSION array
+  SUBMISSION.push({ problemId, solution, isAccepted });
+
+  // Return success message with submission status
+  const status = isAccepted ? 'accepted' : 'rejected';
+  return res.status(201).json({ status });
 });
+
+app.post('/problems', function(req, res) {
+  // Add a new problem to the PROBLEMS array
+  const { title, description, difficulty } = req.body;
+
+  // Check if the user making the request is an admin
+  const isAdmin = req.user && req.user.isAdmin;
+
+  if (!isAdmin) {
+    return res.status(403).send('Forbidden');
+  }
+
+  // Generate a new problem ID (for demo purposes only)
+  const problemId = PROBLEMS.length + 1;
+
+  // Add new problem to PROBLEMS array
+  PROBLEMS.push({ problemId, title, description, difficulty });
+
+  // Return success message
+  return res.status(201).send('Problem created successfully');
+});
+
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
