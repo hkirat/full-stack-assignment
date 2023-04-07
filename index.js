7de0974a83aac5c89d776909a56a11be6232d10f
Shree <shree912@yahoo.com>
2023-04-07 11:10:44 -0700

    backend for leetcode assignemtn project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const e = require('express');
 const express = require('express')
 const app = express()
 const port = 3001
+const bodyParser = require('body-parser'); // Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const USERS = [];
 
@@ -19,54 +22,76 @@ const SUBMISSION = [
 ]
 
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+  console.log(req.body);
+  let email = req.body.email;
+  let password = req.body.password;
+  
+console.log(email+" "+password);
+  if(!email||!password)
+    res.status(400).send("Invalid input");
+  if(!USERS.find(u=>u.email===email)){
+    USERS.push({email,password});
+    res.status(200).send("New User created");
+  }
+  else
+    res.status(200).send("User already exists");
 })
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  let email = req.body.email;
+  let password = req.body.password;
+  if(!email||!password)
+    res.status(400).send("Invalid input");
+  let loggedinuser=USERS.find(u=>u.email===email);
+  if(!loggedinuser
+  || loggedinuser && loggedinuser.password!=password)
+  res.status(401).send('You are unauthorized'); 
+  else
+  res.status(200).send('You are loggedin.'); 
+})
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+app.get('/users', function(req, res) {
+  res.status(200).send(USERS);
 })
 
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.status(200).send(QUESTIONS);
 })
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+   res.status(200).send(SUBMISSION);
 });
 
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+   let email = req.body.email;
+   let code = req.body.code;
+   let question = req.body.question;
+   let substatus=Math.random() < 0.5;
+    SUBMISSION.push({
+      question,email,code,substatus
+    })
+  res.status(200).send("Submitted successfully")
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+
+app.post("/questions/add", function(req, res) {
+  let email = req.body.email;
+  let description = req.body.description;
+  let title = req.body.title;
+  let testCases = req.body.testCases;
+  if(!email || !title)
+    res.status(400).send("Invalid input");
+  if(email!="admin@email.com")
+    res.status(401).send("You are unauthorised to add question");
+  else{
+    QUESTIONS.push({
+      title,description,testCases
+    });
+    res.status(200).send("Question added successfully")
+  }
+ 
+});
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
