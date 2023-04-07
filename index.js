e07c05e63f88b50b07807c3ce38e6b90bbfc69df
Devesh Yadav <ydevesh136@gmail.com>
2023-04-07 20:09:26 +0530

    full stack assignment 1 completed

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.urlencoded({extended:false})) //helps to decode body
 const USERS = [];
+
+const Admins=[
+  {
+    email: "rohan@gmail.com",
+    password: "12345"
+  }
+]
 
 const QUESTIONS = [{
     title: "Two states",
@@ -17,56 +25,103 @@ const QUESTIONS = [{
 const SUBMISSION = [
 
 ]
-
+  
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  var tempemail=req.body["email"];
+  var temppass=(req.body["password"]);
 
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+  var tempuser={ email: tempemail,password:temppass};
+  for (let i=0;i<USERS.length;i++) 
+  {
+    if(USERS[i].email===tempemail) 
+    {
+      res.status(409).send("User already registered");
+      return;
+    }
+  }   
+  USERS.push(tempuser);
+  res.status(200).send(`account created successfully`);
 })
+  
+app.post('/login', function(req, res) 
+{
+  var tempemail=req.body["email"];
+  var temppass=req.body["password"];
+  for (let i=0;i<USERS.length;i++) {
+    if(USERS[i].email===tempemail && USERS[i].password===temppass) {
+      res.setHeader("Authorisation",`${tempemail+"00"}`);
+      res.status(200).send(`login successful`);
+      return;
+    }
+  }
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  res.status(401).send("login credentials are invalid");
 })
 
 app.get('/questions', function(req, res) {
+  var text=``;
+  var count=1;
+  for(let i=0;i<QUESTIONS.length;i++){
+    var temp=QUESTIONS[i];
+    text+=`Ques.${count++}-  Title: ${temp.title}
+Description: ${temp.description}
+Testcases: `;
+    var test="";
+    for(let j=0;j<temp.testCases.length;j++){
+      var obj=temp.testCases[j];
+      test+=`\n-> Input: ${obj.input}
+   Outupt: ${obj.output}`
+    }
+    text+=test+"\n\n";
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  }
+  console.log(text);
+  res.status(200).send(text)
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions", function(req, res) 
+{
+  var str="";
+  var temp=1;
+  for(let i=0;i<SUBMISSION.length;i++){
+    str+=`Submission ${temp++}: ${SUBMISSION[i].check?'accepted':'rejected'}, Time: ${SUBMISSION[i].time}\n`
+  }
+  res.status(200).send(str);
 });
 
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  var { prob, sol } = req.body;
+  var check = Math.random() < 0.5;
+  
+  var submission = { prob, sol, check ,time:new Date().toLocaleString()};
+  SUBMISSION.push(submission);
+  res.status(200).send(`your solution is ${check?'accepted':'rejected'}`);
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+
+app.post("/admin", (req,res)=>{
+  var tempemail=req.body["email"];
+  var temppass=req.body["password"];
+  var x=Admins.find( (item)=>{
+    return item.email===tempemail && item.password===temppass;
+  } );
+  if(x==undefined){
+    res.status(401).send(`invalid credentials`);
+    return;
+  }
+  var ques={
+    title: "Two sum",
+    description: "Given an array , return the maximum two sum of the array?",
+    testCases: [{
+        input: "[1,2,3,4,5]",
+        output: "9"
+    }]
+  }
+  QUESTIONS.push(ques);
+
+  res.status(200).send(`question added successfully`);
+})
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
