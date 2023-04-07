84783eb732d87771b438ffb24f12c32a2f53f2fd
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-08 00:58:50 +0545

    created leetcode question extractor bot

--- utils/leetcodeQuestionExtractor.js
+++ utils/leetcodeQuestionExtractor.js
@@ -3,7 +3,7 @@ const fetch = (...args) =>
 const catchAsync = require("./catchAsync");
 
 //extractor accepts leetcode question url as a parameter and returns nicely formatted question of the url ,
-const extractor = catchAsync(async (url) => {
+const extractor = async (url) => {
   //extracting slug out of url :
   const slug = url.split("/")[4];
   const query = JSON.stringify({
@@ -19,8 +19,6 @@ const extractor = catchAsync(async (url) => {
     headers: { "Content-Type": "application/json" },
   });
   const data = await result.json();
-
   return data.data.question.content;
-});
-
+};
 module.exports = extractor;
