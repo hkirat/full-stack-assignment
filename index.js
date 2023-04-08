f3f9e965c0efcc59bad668a5314070651ffd33b5
veerabrahmam952 <veerabrahmam952@gmail.com>
2023-04-08 13:49:00 +0530

    Added Helper file contains all routes and env file

--- index.js
+++ index.js
@@ -1,73 +1,129 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+require('dotenv').config();
+const helper = require('./helper.js');
+const app = express();
 
-const USERS = [];
-
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
-
-
-const SUBMISSION = [
-
-]
 
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+  //signUp method will responsible for signup implementation
+  const resp = helper.signUp(req);
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if email already exists
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'User registration unsuccessful,try after some time.'
+    });
+  }
 })
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  //login method will responsible for login implementation
+  const resp = helper.login(req);
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if email and password does not match
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'User login unsuccessful,try after some time.'
+    });
+  }
 })
 
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  //getQuestions method will responsible for get questions implementation
+  const resp = helper.getQuestions();
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if Questions empty
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'Loading questions unsuccessful,try after some time.'
+    });
+  }
 })
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  //getSubmissions method will responsible for get submission for the problem implementation
+  const resp = helper.getSubmissions();
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if email already exists
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'Loading submissions unsuccessful,try after some time.'
+    });
+  }
 });
 
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  //postSubmission method will responsible for post submission for the problem implementation
+  const resp = helper.postSubmission(req);
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if submissions empty
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'Posting submissions unsuccessful,try after some time.'
+    });
+  }
 });
 
 // leaving as hard todos
+const isAdmin = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization === 'admin') {
+    next();
+  } else {
+    res.status(401).json({
+      error: true,
+      messgae: 'Only Admins can Add the problem.'
+    });
+  }
+};
+
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
+app.post('/problems', isAdmin, (req, res)=> {
+  //postSubmission method will responsible for post the problem implementation
+  const resp = helper.adminAddProblem(req);
+  if(!resp.error){
+    // return back 200 status code to the client
+    res.status(200).json(resp);
+  }else if(resp.error){
+    // return back 401 status code to the client if Problem is empty
+    res.status(401).json(resp);
+  }else if(resp === null){
+    res.status(407).json({
+      error: true,
+      message: 'Posting Question is unsuccessful,try after some time.'
+    });
+  }
+});
 
+//Read the port number from env file
+const port = process.env.PORT || "3001";
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
