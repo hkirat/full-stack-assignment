9059605512b379b9b0d1342c5151063bab114c6f
arungk703 <100412061+arungk703@users.noreply.github.com>
2023-04-08 16:31:46 +0530

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3001
 
-const USERS = [];
+const USERS = [
+  {
+    name: "Arun",
+    email: "qwerty@123.com",
+    password: "123456",
+    role: "admin",
+    submissions: 0,
+    questions:[],
+    submissions: []
+  }
+];
 
 const QUESTIONS = [{
+    id: 1,
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
     testCases: [{
         input: "[1,2,3,4,5]",
         output: "5"
     }]
-}];
+},
+{
+  id: 2,
+  title: "Distinct States",
+  description: "Given an array , return the Distinct elements of the array?",
+  testCases: [{
+      input: "[1,1,2,2,3,3,4,5,6,3,7,8,9,9]",
+      output: "[4,5,6,7,8]"
+  }],
+}
+];
 
 
 const SUBMISSION = [
@@ -22,51 +46,88 @@ app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(USERS.find(user => user.email === email)){
+    res.send("User already exists");
+  }
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
+  USERS.push({name,email,password});
 
   // return back 200 status code to the client
-  res.send('Hello World!')
+  res.status(200).send('User created successfully');
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const email = req.body.email;
+  const password = req.body.password;
+
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
+  const user = USERS.find(user => user.email === email);
+  if(user == null){
+    res.sendStatus(400).send("User not exists Please Signup.");
+  }
+  if(user.password == password){
+    res.sendStatus(200).send("waidqoadn282qeu3owifbiasndownaldnwalwiahdiawszbxiaksisi");
+  }
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
 
-
-  res.send('Hello World from route 2!')
+  res.sendStatus(401)
 })
 
 app.get('/questions', function(req, res) {
 
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.send(QUESTIONS);
 })
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  res.send(USERS.submissions);
 });
 
 
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+   const email = req.body.email;
+   const questionId = req.body.questionId;
+   const isSolved = req.body.isSolved;
+   if(isSolved){
+    USERS.get(email).QUESTIONS.push(questionId,isSolved);
+   }
+   USERS.get(email).SUBMISSION.push(questionId,isSolved);
+   SUBMISSION.push({email,questionId,isSolved});
+  res.sendStatus(200)
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
+
+app.post("/add-questions", function(req,res){
+
+  const email = req.body.email;
+  const user = USERS.find(user => user.email === email);
+  const question = req.body.question;
+  if(user.role == 'admin'){
+    QUESTIONS.push(question);
+    res.sendStatus(200).send("Question added successfully");
+  }
+  res.sendStatus(401).send("You dont have access to add question");
+
+})
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
