4bdc33ca57e5dcb1e792da99602241bfce6a3914
Mk4Levi <120794789+Mk4Levi@users.noreply.github.com>
2023-04-08 15:34:43 +0530

    update index.js and completing assignment

--- src/index.js
+++ src/index.js
@@ -1,73 +1,143 @@
-const express = require('express')
-const app = express()
-const port = 3001
+// requiring npm modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const hbs = require("hbs");
 
-const USERS = [];
+const app = express();
 
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+// requiring my mongoDB-Atlas 
+require("./db/conn");
+const UserCollection = require("./db/users");
 
+const { json } = require("express");
 
-const SUBMISSION = [
+const port = process.env.PORT || 3000;
 
-]
+const static_path = path.join(__dirname, "../public");
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+// app.use()
+app.use(express.static(static_path));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+// app.set()
+app.set("view engine", "hbs");
+app.set("views", templates_path);
+hbs.registerPartials(partials_path);
 
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+// app.get() for /index (home) page
+app.get("/", function (req, res) {
+  // for Code, refer to path- /templates/views/index.hbs
+  res.render("index");
+});
+
+// app.get() for /signup page
+app.get("/signup", function (req, res) {
+  // for Code, refer to path- /templates/views/signup.hbs
+  res.render("signup");
+});
+
+// app.get() for /login page
+app.get("/login", function (req, res) {
+  // for Code, refer to path- /templates/views/login.hbs
+  res.render("login");
+});
 
 
-  res.send('Hello World from route 2!')
-})
+// app.post() for /signup page
+app.post("/signup", async function (req, res) {
+  try {
+    const userPassword = req.body.password;
+    const userConfirmPassword = req.body.confirmPassword;
 
+    if (userPassword === userConfirmPassword) {
+      const registerUser = new UserCollection({
+        dbName: req.body.name,
+        dbEmail: req.body.email,
+        dbPassword: userPassword,
+        dbConfirmPassword: userConfirmPassword,
+      });
+
+      const registered = await registerUser.save();
+      res.status(201).render("login");
+    } else {
+      res.send("<h1>Passwords are not matching</h1>");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+// app.post() for /login page
+app.post("/login", async function (req, res) {
+  try {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+
+    const findEmail = await UserCollection.findOne({ dbEmail: userEmail });
+
+    if (findEmail.dbPassword === userPassword) {
+      res.status(201).render("questions");
+    } else {
+      res.send("<h1>Invalid Login Details</h1>");
+    }
+  } catch (error) {
+    res.status(400).send("<h1>Invalid Details</h1>");
+  }
+});
+
+
+// app.get() for /questions page 
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  // return the user all the questions 
+  // for Code, refer to path- /templates/views/questions.hbs
+  res.render("questions");
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+
+// app.get() for /submission page 
+app.get("/submission", function(req, res) {
+  // return the users submissions for this problem
+  // for Code, refer to path- /templates/views/submission.hbs
+  res.render("submission");
+});
+
+// app.post() for /submission page 
+app.post("/submission", async function (req, res) {
+  // let the user submit a problem, randomly accept or reject the solution :-
+  try {
+    randomNum = Math.round(Math.random() * 5 );
+    if (randomNum === 1 || randomNum === 3 || randomNum === 5) {
+      res.render("accepted");
+    } else if (randomNum === 0 || randomNum === 2 || randomNum === 4) {
+      res.render("rejected");
+    }
+  } catch (error) {
+    res.status(400).render("rejected");
+  }
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+// for /accepted route
+app.post("/accepted", function(req, res) {
+  // for Code, refer to path- /templates/views/accepted.hbs 
+  res.render("accepted");
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+// for /rejected route
+app.post("/rejected", function(req, res) {
+  // for Code, refer to path- /templates/views/rejected.hbs
+  res.render("rejected");
+});
+
+
+// app.listen() for this Backend-Server 
+app.listen(port, function (req, res) {
+  console.log(`Server running on port: ${port}`);
+});
