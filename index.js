11c1a6c1e95bc886c12e063c3cc329622b2b317a
kritikagoyal <goyalkritika40@gmail.com>
2023-04-08 11:44:32 +0530

    Finished assignment of video 1 of full stack roadmap

--- index.js
+++ index.js
@@ -5,69 +5,108 @@ const port = 3001
 const USERS = [];
 
 const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+    input: "[1,2,3,4,5]",
+    output: "5"
+  }]
+},
+{
+  title: "Reverse a string",
+  description: "Given a string, return the string in reverse order.",
+  testCases: [
+    {
+      input: "hello world",
+      output: "dlrow olleh"
+    }
+  ]
 }];
-
 
 const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
+app.post('/signup', function (req, res) {
+  const { email, password } = req.body;
+  const userExists = USERS.find(user => user.email === email);
+  if (userExists) {
+    return res.status(409).send('User already exists');
+  }
+  USERS.push({ email, password });
+  res.sendStatus(200);
   res.send('Hello World!')
 })
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+app.post('/login', function (req, res) {
+  const { email, password } = req.body;
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const user = USERS.find(user => user.email === email);
 
+  if (!user) {
+    return res.status(401).send('Invalid email or password');
+  }
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  if (user.password !== password) {
+    return res.status(401).send('Invalid email or password');
+  }
 
-
+  const token = 'random_token_string';
+  res.status(200).send({ token });
   res.send('Hello World from route 2!')
 })
 
-app.get('/questions', function(req, res) {
+app.get('/questions', function (req, res) {
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.send(QUESTIONS);
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions", function (req, res) {
+  const problemId = req.query.problemId;
+
+  const submissions = SUBMISSIONS.filter((submission) => {
+    return submission.problemId === problemId;
+  });
+
+  res.send(submissions);
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.post("/submissions", function (req, res) {
+  const { user, problemId, solution } = req.body;
+  const isAccepted = Math.random() < 0.5;
+
+  const submission = { user, problemId, solution, isAccepted };
+  SUBMISSION.push(submission);
+
+  if (isAccepted) {
+    res.status(200).send({ message: "Solution accepted!" });
+  } else {
+    res.status(400).send({ message: "Solution rejected!" });
+  }
 });
 
 // leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+app.post('/problems', function (req, res) {
+  const { title, description, testCases } = req.body;
 
-app.listen(port, function() {
+  if (req.user.isAdmin) {
+    const problem = {
+      title: title,
+      description: description,
+      testCases: testCases
+    };
+
+    QUESTIONS.push(problem);
+
+    res.send('New problem added successfully');
+  } else {
+    res.status(401).send('Only admin users are allowed to add new problems');
+  }
+});
+
+
+app.listen(port, function () {
   console.log(`Example app listening on port ${port}`)
 })
+
