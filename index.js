8aa6efdb78c6d53b56402762da6fd5a05132272d
Yash <kumaryashcse@gmail.com>
2023-04-09 21:46:17 +0530

    basic get/post

--- index.js
+++ index.js
@@ -1,73 +1,150 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
+app.use(express.json());
+
+const SUBMISSIONS = {};
 const USERS = [];
-
-const QUESTIONS = [{
+const LOGGED_IN_USERS = [];
+const QUESTIONS = [
+  {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+        output: "5",
+      },
+    ],
+  },
+];
 
-
-const SUBMISSION = [
-
-]
-
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+/**
+ * Request body format:
+ *
+ *    {
+ *      email: "...",
+ *      password: "..."
+ *    }
+ */
+app.post("/signup", function (req, res) {
+  const userDetails = req.body;
+  if (
+    Object.keys(userDetails).length !== 2 ||
+    !(userDetails.email && userDetails.password)
+  ) {
+    res.status(400).send("Invalid request");
+  } else if (USERS.find((user) => user.email === userDetails.email)) {
+    res.status(400).send("User already exists");
+  } else {
+    USERS.push(userDetails);
+    res.status(200).send(`User added successfully: ${userDetails.email}`);
+  }
 });
 
+/**
+ * Request body format:
+ *
+ *    {
+ *      email: "...",
+ *      password: "..."
+ *    }
+ */
+app.post("/login", function (req, res) {
+  const userDetails = req.body;
+  if (
+    Object.keys(userDetails).length !== 2 ||
+    !(userDetails.email && userDetails.password)
+  ) {
+    res.status(400).send("Invalid request");
+  } else {
+    const user = USERS.find((user) => user.email === userDetails.email);
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+    if (user && user.password === userDetails.password) {
+      const token = userDetails.email + Math.random() * 1000;
+      LOGGED_IN_USERS.push({ email: user.email, token: token });
+      res
+        .status(200)
+        .send(
+          `User logged in successfully: ${userDetails.email}.\n Token: ${token}`
+        );
+    } else {
+      res.status(401).send("Invalid email or password");
+    }
+  }
+});
+
+app.get("/questions", function (req, res) {
+  res.status(200).json(QUESTIONS);
+});
+
+/**
+ * Request body format:
+ *
+ *    {
+ *      email: "..."
+ *    }
+ */
+app.get("/submissions", function (req, res) {
+  const user = req.body;
+  if (Object.keys(user).length !== 1 || !user.email) {
+    res.status(400).send("Invalid request");
+  } else if (!SUBMISSIONS[user.email]) {
+    res.status(400).send("Submission not found for the user");
+  } else {
+    res.status(200).json(SUBMISSIONS[user.email]);
+  }
+});
+
+/**
+ * Request body format:
+ *
+ *    {
+ *      token: "...",     // the token received on login
+ *      submission: {
+ *          title: "...",
+ *          solution: {}
+ *      }
+ *    }
+ */
+app.post("/submissions", function (req, res) {
+  const submission = req.body;
+  if (
+    Object.keys(submission).length !== 2 ||
+    !(submission.token && submission.submission)
+  ) {
+    res.status(400).send("Invalid request");
+  } else {
+    const user = LOGGED_IN_USERS.find(
+      (user) => user.token === submission.token
+    );
+
+    if (user) {
+      if (!QUESTIONS.find((q) => q.title === submission.submission.title)) {
+        res.status(400).send("Question doesn't exist");
+      } else {
+        const random = Math.random();
+        if (random > 0.5) {
+          SUBMISSIONS[user.email] = submission.submission;
+          console.log(SUBMISSIONS);
+          res
+            .status(200)
+            .send(`Submission successful: ${submission.submission}.`);
+        } else {
+          res.status(400).send("Incorrect solution.");
+        }
+      }
+    } else {
+      res.status(401).send("SignUp / Login to submit solutions");
+    }
+  }
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
