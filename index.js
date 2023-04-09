ecb71df5b02367a44e3072bbac0fc4cfd5aa342d
Nachiket Gomkale <nachiketgomkale15@gmail.com>
2023-04-09 12:49:42 +0530

    Snach13: Full stack assignment completed

--- index.js
+++ index.js
@@ -1,73 +1,118 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
 const USERS = [];
 
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+        output: "5",
+      },
+    ],
+  },
+];
 
+const SUBMISSION = [];
 
-const SUBMISSION = [
-
-]
-
-app.post('/signup', function(req, res) {
+app.post("/signup", function (req, res) {
   // Add logic to decode body
   // body should have email and password
-
+  const { email, password } = req.body;
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  const user = USERS.find((user) => user.email === email);
+  if (user) {
+    res.status(400).send("User already exists");
+  } else {
+    USERS.push({ email, password });
+    res.sendStatus(200);
+  }
+});
 
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
+app.post("/login", function (req, res) {
   // Add logic to decode body
   // body should have email and password
+  const { email, password } = req.body;
 
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  const user = USERS.find((user) => user.email === email);
+  if (!user || user.password !== password) {
+    res.status(401).send("Invalid credentials");
+  } else {
+    // If the password is the same, return back 200 status code to the client
+    // Also send back a token (any random string will do for now)
+    const token = "randomstring123";
+    res.status(200).json({ token });
+  }
 });
 
+app.get("/questions", function (req, res) {
+  //return the user all the questions in the QUESTIONS array
+  res.json(QUESTIONS);
+});
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.get("/submissions", function (req, res) {
+  // return the users submissions for this problem
+  res.json(SUBMISSION);
+});
+
+app.post("/submissions", function (req, res) {
+  // let the user submit a problem, randomly accept or reject the solution
+  // Store the submission in the SUBMISSION array above
+  const { title, description, input, output } = req.body;
+
+  // Randomly accept or reject the solution
+  const isAccepted = Math.random() >= 0.5;
+
+  // Create a new submission object
+  const submission = {
+    title,
+    description,
+    input,
+    output,
+    isAccepted,
+  };
+
+  // Store the submission in the SUBMISSION array
+  SUBMISSION.push(submission);
+
+  // Send back the submission object as response
+  res.status(200).json(submission);
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
+app.post("/problems", function (req, res) {
+  // Check if the request contains a valid admin token in the headers
+  const token = req.headers.authorization;
+  if (token !== "Bearer admin_token") {
+    return res.status(401).send("Unauthorized");
+  }
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  // Extract problem details from the request body
+  const { title, description, testCases } = req.body;
+
+  // Create a new problem object
+  const problem = {
+    title,
+    description,
+    testCases,
+  };
+
+  // Add the problem to the QUESTIONS array
+  QUESTIONS.push(problem);
+
+  // Return a success response with the new problem object
+  res.status(200).json(problem);
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
