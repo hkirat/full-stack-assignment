b15e89242cd2db4538a94bd5a914ca198ffa2229
Kohli <yash.kohli@sap.com>
2023-04-11 18:54:51 +0530

    Completed Backend Implementation

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+ exports.initQUESTIONS = [{
+    title: "Two states",
+    description: "Given an array , return the maximum of the array?",
+    testCases: [{
+        input: "[1,2,3,4,5]",
+        output: "5"
+    }],
+    
+},
+{
+  title: "Two Sum",
+    description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
+    testCases: [{
+        input: "[2,7,11,15], target = 9",
+        output: "[0,1]"
+    }]
+}];
+
