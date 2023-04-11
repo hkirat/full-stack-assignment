fe460746cbd2ff4bf7fa1f1f9af0b34d68a820b0
Adithyan A <deepaadithyan56@gmail.com>
2023-04-11 23:39:45 +0530

    Basic Routes with static data added

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
-// Array of user objects with id, username, email, password, and isAdmin properties
+// Array of user objects with id, name, email, password, and isAdmin properties
 const USERS = [
   {
     id: "abc-0",
-    username: "captain_sparkles",
+    name: "captain_sparkles",
     email: "john_doe@example.com",
     password: "secret",
     isAdmin: false
   },
   {
     id: "abc-1",
-    username: "queen_of_the_universe",
+    name: "queen_of_the_universe",
     email: "jane_doe@example.com",
     password: "password123",
     isAdmin: true
   },
   {
     id: "abc-2",
-    username: "the_dancing_dragon",
+    name: "the_dancing_dragon",
     email: "jim_smith@example.com",
     password: "qwerty",
     isAdmin: false
   },
   {
     id: "abc-3",
-    username: "king_of_the_castle",
+    name: "king_of_the_castle",
     email: "admin@example.com",
     password: "admin123",
     isAdmin: true
@@ -69,95 +69,178 @@ const QUESTIONS = [
 ];
 
 // An array of objects representing code submissions by users for each question
-const SUBMISSION = [
+const SUBMISSIONS = [
   {
     questionId: "xyz-0",
     userId: "abc-0",
     time: "2023-04-10T12:30:00Z",
-    solution: "function reverseString(str) {\n  return str.split('').reverse().join('');\n}"
+    solution: "function reverseString(str) {\n  return str.split('').reverse().join('');\n}",
+    isAccepted: false
   },
   {
     questionId: "xyz-1",
     userId: "abc-1",
     time: "2023-04-10T13:15:00Z",
-    solution: "function findLongestWord(str) {\n  const words = str.split(' ');\n  let longestWord = '';\n  for (const word of words) {\n    if (word.length > longestWord.length) {\n      longestWord = word;\n    }\n  }\n  return longestWord;\n}"
+    solution: "function findLongestWord(str) {\n  const words = str.split(' ');\n  let longestWord = '';\n  for (const word of words) {\n    if (word.length > longestWord.length) {\n      longestWord = word;\n    }\n  }\n  return longestWord;\n}",
+    isAccepted: true
   },
   {
     questionId: "xyz-2",
     userId: "abc-2",
     time: "2023-04-10T14:00:00Z",
-    solution: "function factorial(num) {\n  if (num === 0 || num === 1) {\n    return 1;\n  }\n  return num * factorial(num - 1);\n}"
+    solution: "function factorial(num) {\n  if (num === 0 || num === 1) {\n    return 1;\n  }\n  return num * factorial(num - 1);\n}",
+    isAccepted: false
   },
   {
     questionId: "xyz-3",
     userId: "abc-0",
     time: "2023-04-10T14:45:00Z",
-    solution: "function isPalindrome(str) {\n  const reversedStr = str.split('').reverse().join('');\n  return str === reversedStr;\n}"
+    solution: "function isPalindrome(str) {\n  const reversedStr = str.split('').reverse().join('');\n  return str === reversedStr;\n}",
+    isAccepted: true
   },
   {
     questionId: "xyz-4",
     userId: "abc-3",
     time: "2023-04-10T15:30:00Z",
-    solution: "function fibonacci(n) {\n  if (n <= 1) {\n    return n;\n  }\n  return fibonacci(n - 1) + fibonacci(n - 2);\n}"
+    solution: "function fibonacci(n) {\n  if (n <= 1) {\n    return n;\n  }\n  return fibonacci(n - 1) + fibonacci(n - 2);\n}",
+    isAccepted: true
   },
   {
     questionId: "xyz-3",
     userId: "abc-1",
     time: "2023-04-10T16:15:00Z",
-    solution: "function isPalindrome(str) {\n  let left = 0;\n  let right = str.length - 1;\n  while (left < right) {\n    if (str[left] !== str[right]) {\n      return false;\n    }\n    left++;\n    right--;\n  }\n  return true;\n}"
+    solution: "function isPalindrome(str) {\n  let left = 0;\n  let right = str.length - 1;\n  while (left < right) {\n    if (str[left] !== str[right]) {\n      return false;\n    }\n    left++;\n    right--;\n  }\n  return true;\n}",
+    isAccepted: true
   }
 ];
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+
+// MIDDLEWARES
+// Parse JSON request body as JS objects and adds it to 'req.body' object used in routes
+app.use(express.json());
 
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+// ROUTES
+// Adds a new user to the USERS array, only if the user doesn't exists
+app.post("/signup", function (req, res) {
+  // Get name, email and password from request body
+  const { name, email, password } = req.body;
 
+  // Check if mandatory fields are present
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Name, email and password are mandatory" });
+  }
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
+  // Check if user with the same email already exists
+  const userExists = USERS.find((user) => user.email === email);
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "User with the same email already exists" });
+  }
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  // Create new user object
+  const newUser = { name, email, password };
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  // Add new user to USERS array
+  USERS.push(newUser);
 
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  // Return success response with status code 200
+  res.status(200).json({ message: "User created successfully" });
 });
 
+// Send backs auth-token is login is successfull
+app.post("/login", function (req, res) {
+  // Get email and password from request body
+  const { email, password } = req.body;
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  // Check if email and password are present
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are mandatory" });
+  }
+
+  // Check if user with the given email exists in the USERS array
+  const user = USERS.find((user) => user.email === email);
+  if (!user) {
+    return res.status(401).json({ error: "User not found" });
+  }
+
+  // Check if password is correct
+  if (user.password !== password) {
+    return res.status(401).json({ error: "Incorrect password" });
+  }
+
+  // Generate a random authentication token (for now)
+  const authToken = Math.random().toString(36).substring(2);
+
+  // Send back success response with status code 200 and auth token
+  res.status(200).json({ message: "Login successful", authToken: authToken });
+});
+
+// Send back all the questions in the questions array
+app.get("/questions", function (req, res) {
+  res.status(200).json(QUESTIONS);
+});
+
+// Sends back all submissions of a question
+app.get("/submissions", function (req, res) {
+  // Extracting needed arguments from the query
+  const { questionId } = req.body;
+
+  // If questionId arg is absent send back error
+  if (!questionId) {
+    return res
+      .status(400)
+      .json({ error: "Argument questionId is a mandatory field" });
+  }
+
+  // Otherwise, if questionId arg is present send all the submissions for that question
+  const submissions = SUBMISSIONS.filter(
+    (submission) => submission.questionId === questionId
+  );
+  res.status(200).json(submissions);
+});
+
+// Add a new submission to the SUBMISSIONS array
+app.post("/submissions", function (req, res) {
+  // Extracting needed arguments from the query
+  const { questionId, userId, solution } = req.body;
+
+  // If some of the needed arguments are missing send back error
+  if (!questionId || !userId || !solution) {
+    return res
+      .status(400)
+      .json({ error: "questionId, userId, and solution are mandatory fields" });
+  }
+
+  // Randomly accepting or rejecting the submission
+  const isAccepted = Math.random() < 0.5;
+
+  // Creating a submission object with auto generated time field
+  const submission = {
+    questionId: questionId,
+    userId: userId,
+    time: new Date(),
+    solution: solution,
+    isAccepted: isAccepted,
+  };
+
+  // Adding the new submission to the SUBMISSIONS array
+  SUBMISSIONS.push(submission);
+
+  // Send JSON response with `status` denoting accepted/rejected & `message` showing detailed message
+  res.status(201).json({
+    status: isAccepted ? "accepted" : "rejected",
+    message: `Solution is ${isAccepted ? "accepted" : "rejected"}`,
+  });
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
