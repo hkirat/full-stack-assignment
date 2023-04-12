9611e783fe26b08c3d109678f54733f405509de3
Debjyoti0404 <debsen2003@gmail.com>
2023-04-12 09:13:30 +0530

    implemented login and signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const port = 3001
+const port = 3000
 
 const USERS = [];
 
@@ -13,37 +14,114 @@ const QUESTIONS = [{
     }]
 }];
 
-
 const SUBMISSION = [
 
 ]
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.send(`
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <a href="/signup"><button>Sign up</button></a>
+      <a href="/login"><button>Login</button></a>
+    </body>
+  `)
+})
+
+app.get('/signup', (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Sign Up</title>
+    </head>
+    <body>
+      <form action="signup" method="POST">
+        <input type="text" name="email" placeholder="Enter your mail id">
+        <input type="text" name="password" placeholder="Enter your password">
+        <input type="submit" value="submit">
+      </form>
+    </body>
+  </html>`)
+})
+
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
-
-
+  const {email, password} = req.body
+  let flag = 0;
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
+  for(let i = 0; i < USERS.length; i++)
+  {
+    if(email === USERS[i][0])
+    {
+      res.send(`Enter email is already signed up!!`);
+      flag = 1;
+      break;
+    }
+  }
+  if(!flag)
+    USERS.push([email, password]);
   // return back 200 status code to the client
-  res.send('Hello World!')
+  res.status(200).send(`process done`)
+  
+})
+
+app.get('/login', function(req, res) {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Login</title>
+    </head>
+    <body>
+      <form action="login" method="POST">
+        <input type="text" name="email" placeholder="Enter your mail id">
+        <input type="text" name="password" placeholder="Enter your password">
+        <input type="submit" value="submit">
+      </form>
+    </body>
+  </html>`)
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
-
+  const {email, password} = req.body
+  let flag = 0;
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
+  for(let i = 0; i < USERS.length; i++)
+  {
+    if(email === USERS[i][0])
+    {
+      flag = 1;
+      if(password === USERS[i][1])
+        res.status(200).send(`login successful`);
+      else
+        res.status(401).send(`incorrect password`)
+    }
+  }
 
+  if(!flag)
+    res.status(401).send(`entered email does not exists`)
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
 })
 
 app.get('/questions', function(req, res) {
