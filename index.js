d60291903e3c9ea6deb758edd79dff273184de2b
sushanthshetty7 <sushanthr052@gmail.com>
2023-04-13 00:49:59 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,104 +1,104 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 app.use(express.json());
 
-const USERS = [
-  
-];
+const USERS = [];
 
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+        output: "5",
+      },
+    ],
+  },
+];
 
+const SUBMISSION = [];
 
-const SUBMISSION = [
-
-]
-
-app.post('/signup',function(req, res) {
+app.post("/signup", function (req, res) {
   // Add logic to decode body
   // body should have email and password
-  
-    if(!USERS.find(user=>user.email === req.body.email)){
-       USERS.push({
-        email:req.body.email,
-        password:req.body.password
-      })
-      console.log(USERS)
-      return res.status(200).json("user registerd")
-    }else{
-      return res.status(401).json("user already exists")
 
-    }
-    
-  
-
+  if (!USERS.find((user) => user.email === req.body.email)) {
+    USERS.push({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log(USERS);
+    return res.status(200).json("user registerd");
+  } else {
+    return res.status(401).json("user already exists");
+  }
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
-
   // return back 200 status code to the client
+});
 
-})
-
-app.post('/login', function(req, res) {
+app.post("/login", function (req, res) {
   // Add logic to decode body
-  const User = USERS.find(user=>user.email === req.body.email);
+  const User = USERS.find((user) => user.email === req.body.email);
   // Check if the user with the given email exists in the USERS array
-  !User && res.status(401).json("invalid user")
+  !User && res.status(401).json("invalid user");
   // body should have email and password
 
-  if(User){
+  if (User) {
     if (req.body.password === User.password) {
       // If the password is the same, return back 200 status code to the client
       // Also ensure that the password is the same
-      res.status(200).json("login sucessful")
-    }else{
-      res.status(400).json("incorrect password")
+      // Also send back a token (any random string will do for now)
+      res.status(200).json("login sucessful");
+    } else {
+      // If the password is not the same, return back 401 status code to the client
+      res.status(400).json("incorrect password");
     }
   }
-  
-
-  
-  
-
-
-  
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.json(QUESTIONS);
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  
 });
 
+app.get("/questions", function (req, res) {
+  //return the user all the questions in the QUESTIONS array
+  res.json(QUESTIONS);
+});
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.get("/submissions", function (req, res) {
+  const { problem } = req.query;
+
+  const submissionsForProblem = SUBMISSION.filter(
+    (submission) => submission.problem === problem
+  );
+
+  // Return the users submissions for this problem
+  res.send(submissionsForProblem);
+});
+
+app.post("/submissions", function (req, res) {
+  const { problemId, userId, solution } = req.body;
+
+  if (!problemId || !userId || !solution) {
+    res.status(400).send("Missing required fields in request body");
+    return;
+  }
+
+  const newSubmission = { problemId, userId, solution };
+  SUBMISSION.push(newSubmission);
+
+  // Let the user submit a problem, randomly accept or reject the solution
+  // Store the submission in the SUBMISSION array above
+  res.status(201).send({
+    message: "Submission successful",
+    submissionId: newSubmission.id,
+  });
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
