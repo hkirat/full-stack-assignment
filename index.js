b5bbed9f0678bb7498d570baa2376105ad55c5df
Nitin Chandani <nitin1chandani@gmail.com>
2023-04-13 23:10:08 +0530

    first assignment completed

--- index.js
+++ index.js
@@ -1,73 +1,82 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
+const express = require("express");
+const app = express();
+const port = 3000;
 const USERS = [];
 
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+        output: "5",
+      },
+    ],
+  },
+];
 
+app.use(express.json());
 
-const SUBMISSION = [
-
-]
-
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+const SUBMISSION = [];
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.post("/signup", (req, res) => {
+  res.send("sign up");
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send("email and password are required");
+  } else {
+    const existingUser = USERS.find((user) => user.email === req.body.email);
+    if (existingUser) {
+      res.status(409).send("User already exist");
+    } else {
+      USERS.push({
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.status(201).send("user createed");
+    }
+  }
 });
 
+app.post("/login", (req, res) => {
+  //we took the email and pass from the req body
+  const { email, password } = req.body;
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  if (!email || !password) {
+    return res.status(400).json({ error: "email and pass are required" });
+  }
+  const existingUser = USERS.find((user) => user.email === email);
+  const existingUserPass = USERS.find((pass) => pass.password === password);
+  if (!existingUser) {
+    return res.status(401).send("Invalid email of password");
+  }
+  if (!existingUserPass) {
+    return res.status(401).send("Invalid email or password");
+  }
+  if (existingUser && existingUserPass) {
+    return res.status(201).send("successfully logged in");
+  }
+  const rtoken = existingUser && existingUserPass;
+  if (rtoken) {
+    const token = "some_random_token";
+    res.status(200).json({ token });
+  }
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+app.get("/questions", (req, res) => {
+  res.json({ QUESTIONS });
+});
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/submission", (req, res) => {
+  res.send(SUBMISSION);
+});
+app.post("/submissions", (req, res) => {
+  SUBMISSION.push(req.body.submissions);
+  res.status(200).json({ message: "submission successful" });
+});
+
+app.listen(port, () => {
+  console.log("hello world");
+});
