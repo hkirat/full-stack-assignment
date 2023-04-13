1af24cda1bbb54c8f656983f25a43470930b15ff
Tarak Rupesh <tarakrupesh4@gmail.com>
2023-04-13 10:39:32 +0530

    made changes after review

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex % charactersLength);
   }
   return result;
 }
