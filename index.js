e778d1db4285917d84c0ed1cd608260658a78bd1
Admin <admin@Mac-HR961PHQCN.local>
2023-04-14 01:20:23 +0530

    Code changes for assignment 1

--- index.js
+++ index.js
@@ -14,54 +14,84 @@ const QUESTIONS = [{
 }];
 
 
-const SUBMISSION = [
+const SUBMISSIONS = [
 
 ]
+
+const USERS = []
 
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const { email, password } = req.body;
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  const userExists = USERS.find(user => user.email === email);
+  if(userExists) {
+    return res.send('User already exists');
+  }
 
+  USERS.push({ email, password });
 
   // return back 200 status code to the client
-  res.send('Hello World!')
+  res.status(200).send('User created successfully');
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const { email, password } = req.body;
+
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
+  const userExists = USERS.find(user => user.email === email && user.password === password);
+  if(!userExists) {
+    return res.status(401).send('Invalid credentials');
+  }
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
 
-
-  res.send('Hello World from route 2!')
+  res.status(200).json({ token: generateRandomString(10) });
 })
 
 app.get('/questions', function(req, res) {
-
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.json(QUESTIONS)
 })
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  const { userId, questionId } = req.query;
+
+  const user = USERS.find((user) => user.userId === userId);
+
+  if(!user) {
+    return res.status(404).send("User not found");
+  }
+
+  const userSubmissions = SUBMISSIONS.filter((submission) => {
+    return submission.userId === userId && submission.questionId === questionId;
+  });
+
+  res.status(200).json(userSubmissions);
 });
 
 
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+
+   if((Math.random() * 10) % 2) {
+        SUBMISSIONS.push(req);
+        return res.status(200).send("All test cases passed");
+   } else {
+        return res.status(200).send("Not all test cases passed");
+   }
+
 });
 
 // leaving as hard todos
