298441ab9700301bda9ab0118313f16df0660dab
Samardeep Kajal <samk712003@gmail.com>
2023-04-13 21:41:25 +0530

    Completed First Assignment

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
