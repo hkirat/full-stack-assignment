6e98c8ec35e078b7bf533511ff5138885bcf1806
[Sankhadip-Roy] <[sankhadiproy23@gmail.com]>
2023-04-14 14:19:51 +0530

    sign up and log in implemented

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3001
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
 
 const USERS = [];
 
 const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+    input: "[1,2,3,4,5]",
+    output: "5"
+  }]
 }];
 
 
@@ -18,49 +23,70 @@ const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
 })
 
-app.post('/login', function(req, res) {
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/public/signup.html');
+});
+app.post('/signup', async (req, res) => {
   // Add logic to decode body
   // body should have email and password
+  const { email, password } = req.body;
+
+  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  const existingUser = USERS.find(user => user.email === email);
+  if (existingUser) {
+    return res.status(409).json({ message: 'User already exists' });
+  }
+  USERS.push({ email, password });
+
+  // return back 200 status code to the client
+  res.status(200).json({ message: 'User created successfully' });
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+app.post('/login', function (req, res) {
+  // Add logic to decode body
+  // body should have email and password
+  const { email, password } = req.body;
 
   // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const user = USERS.find(user => user.email === email);
+  if (!user) {
+    return res.status(401).json({ message: 'User not found' });
+  }
 
+  // Also ensure that the password is the same
+  // If the password is not the same, return back 401 status code to the client
+  if (user.password !== password) {
+    return res.status(401).json({ message: 'Incorrect password' });
+  }
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  const token = 'randomToken';
+  res.status(200).json({ message: 'Login sucessful', token })
 })
 
-app.get('/questions', function(req, res) {
+app.get('/questions', function (req, res) {
 
   //return the user all the questions in the QUESTIONS array
   res.send("Hello World from route 3!")
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
+app.get("/submissions", function (req, res) {
+  // return the users submissions for this problem
   res.send("Hello World from route 4!")
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
+app.post("/submissions", function (req, res) {
+  // let the user submit a problem, randomly accept or reject the solution
+  // Store the submission in the SUBMISSION array above
   res.send("Hello World from route 4!")
 });
 
@@ -68,6 +94,6 @@ app.post("/submissions", function(req, res) {
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Example app listening on port ${port}`)
 })
