ecf89288a4d014828efaac38a327d9efcc780ce1
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-04-16 14:08:33 +0530

    Changes made to index.js

--- index.js
+++ index.js
@@ -78,6 +78,10 @@ app.get('/questions', function (req, res) {
 app.get("/submissions/:questionId", function (req, res) {
   // return the users submissions for this problem
   const { questionId } = req.params;
+  const validQuestionId = QUESTIONS.find(qid => QUESTIONS.questionId ===  questionId);
+  if(!validQuestionId){
+    return res.sendStatus(400).json({ error: 'Invalid QuestionId' });
+  }
   const submissions = SUBMISSION.filter(submission => submission.questionId === questionId);
   res.status(200).json({ submissions });
 });
@@ -87,6 +91,10 @@ app.post("/submissions/:questionId", function (req, res) {
   // let the user submit a problem, randomly accept or reject the solution
   // Store the submission in the SUBMISSION array above
   const { questionId } = req.params;
+  const validQuestionId = QUESTIONS.find(qid => QUESTIONS.questionId ===  questionId);
+  if(!validQuestionId){
+    return res.sendStatus(400).json({ error: 'Question does not exist' });
+  }
   const { answer } = req.body;
   const isAccepted = Math.random() >= 0.5;
 
@@ -99,7 +107,7 @@ app.post("/submissions/:questionId", function (req, res) {
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
-app.post("/problems", function (req, res){
+app.post("/problem", function (req, res){
   const { isAdmin } = req.user;
 
   if(!isAdmin){
