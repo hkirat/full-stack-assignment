d502a9d5edbdced66298a04688e84e78e5d042f8
Chayan199916 <swapnomoy199916@gmail.com>
2023-05-01 20:50:19 +0530

    feat : question schema added; role property added;

--- schema.js
+++ schema.js
@@ -4,6 +4,7 @@ const joi = require('joi');
 const emailPasswordSchema = joi.object({
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
+  role: joi.string().optional().default('user')
 });
 // Define the schema for the request payload while storing a submission
 const submissionSchema = joi.object({
@@ -11,5 +12,14 @@ const submissionSchema = joi.object({
   code: joi.string().required(),
   language: joi.string().valid('javascript', 'python').required()
 });
+// Define the schema for the request payload while submitting a question
+const questionSchema = joi.object({
+  title: joi.string().required(),
+  description: joi.string().optional(),
+  testCases: joi.array().items(joi.object({
+    input: joi.string().required(),
+    output: joi.string().required()
+  })).min(1).required()
+});
 
-module.exports = {emailPasswordSchema, submissionSchema};
+module.exports = {emailPasswordSchema, submissionSchema, questionSchema};
