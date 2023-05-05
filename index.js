e343c0271525257d99cf7f26b7321a13d3eb080e
Devesh Yadav <ydevesh136@gmail.com>
2023-05-06 01:32:22 +0530

    some additions

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3001
 
 app.use(express.urlencoded({extended:false})) //helps to decode body
-const USERS = [];
+app.use((req, res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+app.use(express.json());
+const USERS = [
+  {email:"devesh@gmail.com",
+  password:"12345"}
+];
 
 const Admins=[
   {
@@ -11,6 +23,57 @@ const Admins=[
     password: "12345"
   }
 ]
+
+const PROBLEMS=[
+  {
+  title: "Bitwise AND of Numbers Range",
+  difficulty: "Medium",
+  acceptance: "42%"
+  },{
+      title: "Bitwise AND of Numbers Range",
+      difficulty: "Medium",
+      acceptance: "412%"
+  },
+  {
+      title: "202. Happy Number",
+      difficulty: "Easy",
+      acceptance: "54.9%"
+  },
+  {
+      title: "Bitwise AND of Numbers Range",
+      difficulty: "Medium",
+      acceptance: "412%"
+  },
+  {
+      title: "202. Happy Number",
+      difficulty: "Easy",
+      acceptance: "54.9%"
+  },
+  {
+      title: "Bitwise AND of Numbers Range",
+      difficulty: "Medium",
+      acceptance: "412%"
+  },
+  {
+      title: "202. Happy Number",
+      difficulty: "Easy",
+      acceptance: "54.9%"
+  },
+  {
+      title: "Bitwise AND of Numbers Range",
+      difficulty: "Medium",
+      acceptance: "412%"
+  },
+  {
+      title: "202. Happy Number",
+      difficulty: "Easy",
+      acceptance: "54.9%"
+  },
+  {
+      title: "203. Remove Linked List Elements",
+      difficulty: "Hard",
+      acceptance: "42%"
+  }];
 
 const QUESTIONS = [{
     title: "Two states",
@@ -45,16 +108,20 @@ app.post('/signup', function(req, res) {
   
 app.post('/login', function(req, res) 
 {
+
   var tempemail=req.body["email"];
   var temppass=req.body["password"];
+  console.log(tempemail,temppass);
   for (let i=0;i<USERS.length;i++) {
-    if(USERS[i].email===tempemail && USERS[i].password===temppass) {
+    console.log(USERS[i].email, USERS[i].password);
+    if(USERS[i].email==tempemail && USERS[i].password==temppass) {
+      
       res.setHeader("Authorisation",`${tempemail+"00"}`);
       res.status(200).send(`login successful`);
       return;
     }
   }
-
+  console.log("failed");
   res.status(401).send("login credentials are invalid");
 })
 
@@ -77,6 +144,24 @@ Testcases: `;
   }
   console.log(text);
   res.status(200).send(text)
+})
+app.get('/problems/:pagenumber', function(req, res) {
+  res.setHeader("Content-Type","application/json");
+  var {pagenumber}=req.params;
+  var first=(Number(pagenumber)-1)*5;
+  
+  var last=Number(pagenumber)*5;
+  // console.log(first, last);
+  var temparr=[];
+
+  for(let i=first;i<last;i++){
+    // console.log(PROBLEMS[i].title);
+    var obj={title:PROBLEMS[i].title, difficulty:PROBLEMS[i].difficulty, acceptance:PROBLEMS[i].acceptance};
+    temparr.push(obj);
+  }
+  let data=JSON.stringify(temparr);
+  console.log(data);
+  res.status(200).send(data);
 })
 
 app.get("/submissions", function(req, res) 
