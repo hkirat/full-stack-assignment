ee36295b8ce3c53055078ca34e367a079d6a42f9
Ansh-Rajput <anshravikrish425317@gmail.com>
2023-05-07 22:33:37 +0530

    HR assignment completed. all route are working properly.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3001
+
+const express = require('express');
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const KEY = "PriviteKey";//privite key for sign in token
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
 
 const USERS = [];
 
@@ -13,61 +20,120 @@ const QUESTIONS = [{
     }]
 }];
 
+const SUBMISSION = [];
 
-const SUBMISSION = [
+const checkCode = (code) => {//function to check if user's code is correct or not 
+  return Math.floor(Math.random());
+}
 
-]
+const userEmail = (req,res,next)=>{//middleware to find email of user from token.
+  const token = JSON.parse(req.headers.token);
+  const email = jwt.verify(token,KEY);
+  req.user = email;
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  next();
+}
 
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+app.post('/signup',[
+  body("email").isEmail().notEmpty().custom(async (value)=>{//this line check if email alredy exist or not .
+    USERS.forEach((user)=>{
+      if(user.email === value){
+        throw new Error('E-mail already in use');
+      }
+    })
+  }),
+  body("password").notEmpty()
+],(req, res) =>{
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    const tag = `${(req.body.tag)?req.body.tag:"general"}`
+    USERS.push({...req.body,tag:tag});
+    console.log(USERS);
+    const token = jwt.sign(req.body.email,KEY);
+    res.status(200).send(token);
+  }
+  else{
+    res.send({error:'Some error occurred.'})
+  }
 })
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+app.post('/login',[
+  body("email").isEmail().notEmpty(),
+  body("password").notEmpty()
+], (req, res) => {
+  const result = validationResult(req);
+  if(!(result.isEmpty())){
+    res.status(401).send({error:'Some error occurred.'})
+  }
+  USERS.forEach((user)=>{//logic to find user's email in database.
+    if(user.email === req.body.email){
+      if(user.password === req.body.password){
+        const token = jwt.sign(req.body.email,KEY);
+        res.status(200).send(token);
+      }
+      else{
+        res.status(401).send({error:'Incorrect Password.'})
+      }
+    }
+    else{
+      res.status(401).send({error:'Some error occurred.'})
+    }
+  })
 })
 
 app.get('/questions', function(req, res) {
-
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.send({QUESTIONS});
+})
+
+app.post('/questions',userEmail,[//Allow the user to add more queations to QUESTIONS array if user is a admin.
+  body("question").notEmpty()
+], function(req, res) {
+  const result = validationResult(req);
+  if(!(result.isEmpty())){
+    res.status(401).send({error:'Some error occurred.'})
+  }
+  
+  const email = req.user;
+  USERS.forEach((user)=>{
+    if(user.email == email){
+      if(user.tag === "admin"){
+        QUESTIONS.push(req.body.question);
+        console.log(QUESTIONS)
+        res.send("one question added.");
+      }
+      else{
+        res.status(403).send("Access denied")
+      }
+    }
+  })
 })
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+   res.send({SUBMISSION});
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.post("/submissions",[//route to submit submissions .
+  body("code").notEmpty()
+], function(req, res) {
+  const result = validationResult(req);
+  if(!(result.isEmpty())){
+    res.status(401).send({error:'Some error occurred.'})
+  }
+  else if(checkCode(req.body.code)){
+    SUBMISSION.push({code:req.body.code,status:"accepted"});
+    console.log(SUBMISSION);
+    res.send("accepted")
+  }
+  else{
+    SUBMISSION.push({code:req.body.code,status:"rejected"});
+    console.log(SUBMISSION);
+    res.send("rejected");
+  }
 });
-
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
