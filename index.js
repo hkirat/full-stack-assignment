e692de4079975f7fa2d3271d4fa88c12739dee7c
nithinsikinam <nithinsikinam@gmail.com>
2023-05-08 23:10:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3001
+
+function authenticateToken(req,res,next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token == null) return res.sendStatus(401)
+  
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,email) =>{
+      if (err) return res.sendStatus(403)
+      req.email = email
+      next()
+  })
+}  
+
+function getEmailIndex(email) {
+  for (let i = 0; i < USERS.length; i++) {
+    if (USERS[i].email === email) {
+      return i; 
+    }
+  }
+  return -1; 
+}
+
+function isAdmin(email){
+for(let i = 0; i < USERS.length; i++) {
+  if(USERS[i].email === email) {
+    if(USERS[i].role === 'admin') {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+}
+
+function isQuestionExisting(title){
+  for(let i = 0; i < QUESTIONS.length; i++) {
+    if(QUESTIONS[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+}
 
 const USERS = [];
 
@@ -13,61 +59,100 @@ const QUESTIONS = [{
     }]
 }];
 
-
 const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+app.use(express.json());
 
+app.post('/signup',async (req, res) => {
+ var {email, password,role} = req.body;
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+ if(!email || !password){
+  res.status(400).json({status:"INVALID_REQUEST"});
+  return;
+ }
+ const emailIndex=getEmailIndex(email);
 
+ if(!(emailIndex<0)){
+  res.status(400).json({status:"USER_EXISTS"});
+  return;
+ }
+ password = await bcrypt.hash(password, 10);
+ 
+ if(role==='admin'){
+  USERS.push({email,password,role})
+  res.status(200).json({status:"ADDED_NEW_ADMIN"});
+  return;
+}
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
+  USERS.push({email,password});
+  res.status(200).json({status:"ADDED_NEW_USER"});
 })
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+app.post('/login',async (req, res)=> {
+  const {email,password} = req.body;
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  if(!email || !password){
+    res.status(400).json({status:"INVALID_REQUEST"});
+  }
 
+  const IndexOfUser =  getEmailIndex(email); 
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  if(IndexOfUser<0){
+    res.status(400).json({status:"NO_USER"})
+  return;
+  }
+  
+  if(!(await bcrypt.compare(password, USERS[IndexOfUser].password))){
+    res.status(400).json({status:"WRONG_PASSWORD"});
+    return;
+  }
 
+  const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
+  res.status(200).json({ status: 'SUCCESS', accessToken: accessToken });
 
-  res.send('Hello World from route 2!')
+});
+
+app.get('/questions',  (req, res) => {
+  res.status(200).json({QUESTIONS})
 })
 
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions/:title", (req, res) => {
+  const { title } = req.params;
+  const matchingSubmissions = SUBMISSION.filter(submission => submission.title === title);
+  res.status(200).json({matchingSubmissions})
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.post("/submissions",authenticateToken ,(req, res) => {
+  const randomBoolean = Math.random() >= 0.5;
+  if(randomBoolean){
+    const { title, body } = req.body;
+    const email = req.email.email;
+    SUBMISSION.push({title,body,email})
+    res.status(200).json({status:"ADDED_NEW_SUBMISSION"})
+    return;
+  }
+  res.status(400).json({status:"SUBMISSION_REJECTED"});
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
 
-app.listen(port, function() {
+app.post("/questions",authenticateToken,(req,res)=>{
+  const email = req.email.email;
+  if(!isAdmin(email)){
+    res.status(400).json({status:"NOT_ADMIN"});
+    return;
+  }
+  const {question} = req.body;
+  if(isQuestionExisting(question.title)){
+    res.status(400).json({status:"QUESTION_WITH_TITLE_EXISTS"});
+    return;
+  }
+  QUESTIONS.push(question);
+  res.status(200).json({status:"QUESTION_ADDED"})
+})
+
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
