dc7007d904a409a0c8de905a46a8150edcde0556
Sandeep Bhartiya <sandeepbhartiya9087@gmail.com>
2023-05-12 15:33:26 +0530

    new-commit1

--- index.js
+++ index.js
@@ -19,26 +19,24 @@ const QUESTIONS = [
 
 const SUBMISSION = [];
 
+//SignUp Page
+
 app.post("/signup", function (req, res) {
   try {
-    // Add logic to decode body
+    const { email, password, isAdmin } = req.body;
 
-    const { email, password,isAdmin } = req.body;
-
-    // body should have email and password
-    if (!email || !password ||!isAdmin) {
+    if (!email || !password || !isAdmin) {
       return res.status(404).send({
         message: "Email or Password is not Provided",
       });
     }
 
-    //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
     if (USERS.some((user) => user.email == email)) {
       return res.status(400).send({
         message: "Email already Exist!!!",
       });
     } else {
-      USERS.push({ email, password,isAdmin});
+      USERS.push({ email, password, isAdmin });
       res.status(201).send({
         message: "User Created",
       });
@@ -48,124 +46,115 @@ app.post("/signup", function (req, res) {
       message: "Internal Server Error while creating a User",
     });
   }
-
-  // return back 200 status code to the client
-  // res.send('Hello World!')
 });
+
+//Login Page
 
 app.post("/login", function (req, res) {
   try {
-    // Add logic to decode body
-    const {email,password}=req.body;
-    // body should have email and password
-      if(!email ||!password)
-      {
-        return res.status(401).send({
-          message: "Email or Password is not Provided"
-        })
-      }
-    // Check if the user with the given email exists in the USERS array
-    const user=USERS.find(user=>user.email===email)
-    if(!user)
-    {
-        return res.status(401).send({
-          message:"Failed!!! Email does not Exist"
-        })
-    }
-    // Also ensure that the password is the same
-    if(user.password!==password)
-    {
-      return res.status(401).send({
-        message:"Failed!!! Password is inncorrect"
-      })
-    }
-    else
-    {
-      // If the password is the same, return back 200 status code to the client
-      // Also send back a token (any random string will do for now)
-      const token="random-token";
-      res.status(200).send({
-        message:"Login Successfull",
-        token:token
-      })
-    }
-    // If the password is not the same, return back 401 status code to the client
+    const { email, password } = req.body;
 
-   
+    if (!email || !password) {
+      return res.status(401).send({
+        message: "Email or Password is not Provided",
+      });
+    }
+
+    const user = USERS.find((user) => user.email === email);
+    if (!user) {
+      return res.status(401).send({
+        message: "Failed!!! Email does not Exist",
+      });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).send({
+        message: "Failed!!! Password is inncorrect",
+      });
+    } else {
+      const token = "random-token";
+      res.status(200).send({
+        message: "Login Successfull",
+        token: token,
+      });
+    }
   } catch (err) {}
 });
 
+//Question Page
+
 app.get("/questions", function (req, res) {
-  //return the user all the questions in the QUESTIONS array
+ 
   res.json(QUESTIONS);
 });
 
+//Submission Page
+
 app.get("/submissions", function (req, res) {
-  // return the users submissions for this problem
-const problemId=req.query.problemId;
-if(!problemId)
-{
-  return res.status(401).send({
-    message:"Problme Id is not Provided"
-  })
-}
-const submission=SUBMISSION.filter(submission=>submission.problemId===problemId);
-res.json(submission);
-}
-)
+
+  const problemId = req.query.problemId;
+  if (!problemId) {
+    return res.status(401).send({
+      message: "Problme Id is not Provided",
+    });
+  }
+  const submission = SUBMISSION.filter(
+    (submission) => submission.problemId === problemId
+  );
+  res.json(submission);
+});
+
+//Submission
 
 app.post("/submissions", function (req, res) {
-  const {problemId,solution}=req.body;
-  if(!problemId || !solution)
-  {
+  const { problemId, solution } = req.body;
+  if (!problemId || !solution) {
     return res.status(400).send({
-      message:"Failed!!! problemId or soluition is not provided"
-    })
+      message: "Failed!!! problemId or soluition is not provided",
+    });
   }
-  const isAccepted=Math.random()<0.5;
+  const isAccepted = Math.random() < 0.5;
   SUBMISSION.push({
     problemId,
     solution,
-    isAccepted
+    isAccepted,
   });
   res.json({
-    message:isAccepted?"Submission Successfull":"Submission is not Successfull"
-  })
+    message: isAccepted
+      ? "Submission Successfull"
+      : "Submission is not Successfull",
+  });
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
-const isAdmin=(req,res,next)=>{
-  const {isAdmin}=req.user;
-  if(isAdmin){
+const isAdmin = (req, res, next) => {
+  const { isAdmin } = true; //her i wanted to take token where user login but i think i need to used jsonWebToken and a database
+  if (isAdmin) {
     return res.status(400).send({
-      message:"Unauthorized Access! You are not an admin."
-    })
+      message: "Unauthorized Access! You are not an admin.",
+    });
   }
   next();
-}
-app.post("/problems",isAdmin,function(req,res){
-  const {title,description,testCases}=req.body;
-  if(!title || !description || !testCases)
-  {
+};
+app.post("/problems", isAdmin, function (req, res) {
+  const { title, description, testCases } = req.body;
+  if (!title || !description || !testCases) {
     return res.status(400).send({
-      message:"Failed!!! title ,description or testCases is not Provided"
-    })
+      message: "Failed!!! title ,description or testCases is not Provided",
+    });
   }
 
-  const id=Math.random().toString(36).substring(2,8);
+  const id = Math.random().toString(36).substring(2, 8);
   QUESTIONS.push({
     id,
     title,
     description,
-    testCases
+    testCases,
   });
   res.status(201).send({
     id,
-    message:"Problem Created Successfully"
-  })
-})
+    message: "Problem Created Successfully",
+  });
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
