fd38689f8b04abdb255c2abe8a74627fd7f2075d
adithisirpa <adithisirpa@gmail.com>
2023-05-23 01:29:27 +0530

    testing out index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 const USERS = [];
 
 const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+      input: "[1,2,3,4,5]",
+      output: "5"
+  }]
 }];
+
 
 const SUBMISSIONS = [
 
