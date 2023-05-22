57664e9a8a177d0905dff9ed7cd09aca8046d373
Ashish Varma <ashishv7521@gmail.com>
2023-05-23 01:07:14 +0530

    filled the required parts of the code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
+
 
 const USERS = [];
 
+
 const QUESTIONS = [{
     title: "Two states",
-    description: "Given an array , return the maximum of the array?",
+    description: "Given an array, return the maximum of the array?",
     testCases: [{
         input: "[1,2,3,4,5]",
         output: "5"
@@ -14,60 +16,122 @@ const QUESTIONS = [{
 }];
 
 
-const SUBMISSION = [
+const SUBMISSIONS = [];
 
-]
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+
+app.get('/', function(req, res) {
+  res.send('Welcome to the application!');
+});
+
 
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  const { email, password } = req.body;
 
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  const existingUser = USERS.find(user => user.email === email);
 
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
+  if (existingUser) {
+    res.status(409).send('User already exists');
+  } else {
+    const newUser = {
+      email,
+      password
+    };
+    USERS.push(newUser);
+
+
+    res.status(200).send('Signup successful');
+  }
+});
+
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const { email, password } = req.body;
 
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  const user = USERS.find(user => user.email === email);
 
 
-  res.send('Hello World from route 2!')
-})
+  if (!user) {
+    res.status(401).send('Invalid credentials');
+  } else {
+    if (user.password === password) {
+      const token = generateRandomToken();
+
+
+      res.status(200).json({ token });
+    } else {
+      res.status(401).send('Invalid credentials');
+    }
+  }
+});
+
 
 app.get('/questions', function(req, res) {
+  res.status(200).json(QUESTIONS);
+});
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  const userId = req.user.id; // Assuming you have the user ID available in the request object
+
+
+  const userSubmissions = SUBMISSIONS.filter(submission => submission.userId === userId);
+  res.status(200).json(userSubmissions);
 });
 
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  const userId = req.user.id; // Assuming you have the user ID available in the request object
+
+
+  const result = Math.random() < 0.5 ? "accept" : "reject";
+
+
+  const newSubmission = {
+    userId,
+    data: req.body.data,
+    result
+  };
+
+
+  SUBMISSIONS.push(newSubmission);
+
+
+  res.status(200).send("Submission successful");
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+
+// Middleware function for checking admin role
+function checkAdminRole(req, res, next) {
+  // Check if the user is authenticated and has the "admin" role
+  if (req.isAuthenticated() && req.user.role === 'admin') {
+    // User is authenticated and has the "admin" role, proceed to the next middleware or route handler
+    next();
+  } else {
+    // User is either not authenticated or does not have the "admin" role, send a 403 Forbidden status code
+    res.status(403).send('Access denied');
+  }
+}
+
+
+// Route for adding a new problem accessible only to admins
+app.post('/problems', checkAdminRole, function(req, res) {
+  // Logic for adding a new problem goes here
+  res.status(200).send('Problem added successfully');
+});
+
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+function generateRandomToken() {
+  // Implement your token generation logic here
+  // Return a randomly generated token
+}
