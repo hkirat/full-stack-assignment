847f3a87163f999f613811259eaafbc12cf7a583
ashokkumarmuthu <74347124+ashokkumarmuthu@users.noreply.github.com>
2023-05-22 20:30:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const USERS = [
 
 const QUESTIONS = [{
     title: "Two states",
-    description: "Given an array , return the maximum of the array?",
+    description: "Given an array , return the maximum of the array? ",
     testCases: [{
         input: "[1,2,3,4,5]",
         output: "5"
