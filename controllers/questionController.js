cbc59f5a1a281f1b8010ac95048276a6e0124b64
sumeetprajapati1996 <sumeetprajapati15@gmail.com>
2023-05-24 10:22:39 +0530

    Bug fix

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,3 +1,5 @@
+import { QUESTIONS } from "../models/Question.js";
+
 const getAllQuestions = (req, res) => {
   return res.status(200).json({ data: { questions: QUESTIONS } });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import { login, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("/login", login);
-router.get("/signup", signup);
+router.post("/login", login);
+router.post("/signup", signup);
 
 export default router;
