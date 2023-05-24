8a66f38db2a059431384818f89ba6ee830d5fa5b
Jassi <jaswinder.dandyan2000@gmail.com>
2023-05-24 22:00:54 +0530

    added get and post questions

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -3,10 +3,12 @@ const {
   getQuestions,
   addQuestion,
 } = require("../controllers/question.controllers");
+const authMiddleware = require("../middlewares/auth.middleware");
+const isAdmin = require('../middlewares/isAdmin.middleware');
 
 const router = express.Router();
 
 router.get("/", getQuestions);
-router.post("/", addQuestion);
+router.post("/", authMiddleware, isAdmin, addQuestion);
 
 module.exports = router;
