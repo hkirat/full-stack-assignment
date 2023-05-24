623244dcdabd0cf198d7986c017a197ccd70813d
Jassi <jaswinder.dandyan2000@gmail.com>
2023-05-24 19:59:09 +0530

    added logging and error handler middleware

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 const authRouter = require("./src/routes/auth.routes");
 const submissionRouter = require("./src/routes/submission.routes");
 const questionRouter = require("./src/routes/question.routes");
+
+const loggingMiddleware = require("./src/middlewares/logging.middleware.js");
+const errorHandler = require("./src/middlewares/errorHandler.middleware");
 const port = 3001;
 
 app.use(express.json());
+app.use(loggingMiddleware);
 
 app.use("/", authRouter);
 app.use("/submissions", submissionRouter);
 app.use("/questions", questionRouter);
+
+app.use(errorHandler);
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
