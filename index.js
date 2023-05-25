e288699cd82e7acd8b35e874d598f3aba494209a
asbar <asbar03KK@gmail.com>
2023-05-25 12:22:23 +0530

    feat login validation

--- index.js
+++ index.js
@@ -61,44 +61,45 @@ app.post('/signup', (req, res) =>{
   }
   
 })
-// app.get("/login",(req,res)=>{
-//   res.render("login");
-// })
-// app.post('/login', (req, res) =>{
-//   // Add logic to decode body
-//   // body should have email and password
+app.get("/login",(req,res)=>{
+  res.render("login");
+})
+app.post('/login', (req, res) =>{
+  // Add logic to decode body
+  // body should have email and password
 
-//   // Check if the user with the given email exists in the USERS array
-//   // Also ensure that the password is the same
-
-
-//   // If the password is the same, return back 200 status code to the client
-//   // Also send back a token (any random string will do for now)
-//   // If the password is not the same, return back 401 status code to the client
+  // Check if the user with the given email exists in the USERS array
+  // Also ensure that the password is the same
 
 
-//   const usernameInBody  = req.body.username;
-//   const passwordInBody  = req.body.pass;
-//   const validUser = USERS.some(user=> {user.username === usernameInBody && user.password === passwordInBody});
-//   const userExists = USERS.some(user=> user.username === usernameInBody);
+  // If the password is the same, return back 200 status code to the client
+  // Also send back a token (any random string will do for now)
+  // If the password is not the same, return back 401 status code to the client
+
+
+  const usernameInBody  = req.body.username;
+  const passwordInBody  = req.body.pass;
+  const validUser = USERS.some(user=> {user.username === usernameInBody && user.password === passwordInBody});
+  const userExists = USERS.some(user=> user.username === usernameInBody);
   
-//   if(validUser){
-//     console.log("Valid User");
-//     return res.status(200).redirect('questions');
-//   }
-//   if(userExists){
-//     allert("Wrong Password");
-//     return res.sendStatus(401);
-//   }
-//   else{
-//     return res.status(200).redirect('signup');
-//   }
+  if(validUser){
+    console.log("Valid User");
+    return res.status(200).redirect('questions');
+  }
+  if(userExists){
+    allert("Wrong Password");
+    return res.sendStatus(401);
+  }
+  else{
+    return res.status(200).redirect('signup');
+  }
   
-// })
+})
 
 // app.get('/questions', (req, res)=> {
 
 //   //return the user all the questions in the QUESTIONS array
+  
 //   res.send("Hello World from route 3!")
 // })
 
