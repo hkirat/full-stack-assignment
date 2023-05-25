f36444c3ad925a3c6f32921290fb91c31dee92ac
sumeetprajapati1996 <sumeetprajapati15@gmail.com>
2023-05-25 13:47:59 +0530

    Make questions accessible for unauthenticated users

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { ensureAuthenticated } from "../middlewares/authMiddleware.js";
 import { getAllQuestions } from "../controllers/questionController.js";
 
 const router = express.Router();
 
-router.use(ensureAuthenticated);
 router.get("/", getAllQuestions);
 
 export default router;
