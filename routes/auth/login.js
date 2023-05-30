d491ae3185c65ee21381e1ef954d0c1cde7fb4d7
Swastik <swastikpanda32@gmail.com>
2023-05-31 02:04:07 +0530

    Updated logic to the auth routes

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -5,10 +5,13 @@ const app = express();
 app.post('/login', function(req, res) {
     // Add logic to decode body
     // body should have email and password
+
+    const {email,password} = req.body;
   
     // Check if the user with the given email exists in the USERS array
     // Also ensure that the password is the same
-  
+
+    const userExits = USERS.some(user => user.email === email);
   
     // If the password is the same, return back 200 status code to the client
     // Also send back a token (any random string will do for now)
--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -4,10 +4,21 @@ const app = express();
 app.post('/signup', function(req, res) {
     // Add logic to decode body
     // body should have email and password
+
+    const email = req.body.email;
+    const password = req.body.password;
   
   
     //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
   
+    const userExits = USERS.some(user => user.email === email);
+
+    if(userExits) {
+      res.sendStatus(409);
+    } else {
+
+      USERS.push({email,password})
+    }
   
     // return back 200 status code to the client
     res.sendStatus(200);
