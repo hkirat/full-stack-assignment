c9d26f13171696db068fc6ee0d4cbe726a6b0da8
Swastik <swastikpanda32@gmail.com>
2023-06-02 01:38:58 +0530

    imported questions route

--- index.js
+++ index.js
@@ -3,30 +3,24 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000;
 
-const USERS = [];
 
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
 
 
 // Import the route handlers
 const signupRoutes = require('./routes/auth/signup');
 const loginRoutes = require('./routes/auth/login');
+const questionRoutes = require('./routes/questions/questions');
 const submissionRoutes = require('./routes/submissions/submissions');
 const adminRoutes = require('./routes/admin/admin');
 
 // Use the route handlers
 app.use(signupRoutes);
 app.use(loginRoutes);
+app.use(questionRoutes);
 app.use(submissionRoutes);
-//app.use(adminRoutes);
+app.use(adminRoutes);
 app.use(bodyParser.json());
+
 
 
 
