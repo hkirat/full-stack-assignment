f31f2cde0417f4ac729cc0513f8411d3b9462d2e
Swastik <swastikpanda32@gmail.com>
2023-06-02 01:37:43 +0530

    completed logic for signup route

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const router = express.Router();
+const { USERS } = require('../../users');
+
+router.use(bodyParser.json());
 
 router.post("/signup", function (req, res) {
   // Add logic to decode body
@@ -10,16 +14,20 @@ router.post("/signup", function (req, res) {
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
-  const userExits = USERS.some((user) => user.email === email);
-
-  if (userExits) {
-    res.sendStatus(409);
+  if (USERS.length === 0){
+    USERS.push({email,password});
+    res.sendStatus(200);
   } else {
-    USERS.push({ email, password });
+    const userExists = USERS.some((user) => user.email === email);
+    if (userExists) {
+      res.sendStatus(409).send('Email already registered');
+    } else {
+      USERS.push({ email, password });
+      // return back 200 status code to the client
+      res.sendStatus(200);
+    }
+   
   }
-
-  // return back 200 status code to the client
-  res.sendStatus(200);
 });
 
 module.exports = router;
