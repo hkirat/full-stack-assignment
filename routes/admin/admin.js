2b2ce412646b7d1debe6ad8430af1ac49090bfcb
Swastik <swastikpanda32@gmail.com>
2023-06-02 01:40:13 +0530

    added boilerplate to admin route , logic yet to be done

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,3 +2,12 @@
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
+const express = require('express');
+const router =  express.Router();
+
+
+router.get("/users",function(req,res){
+    
+})
+
+module.exports = router;
