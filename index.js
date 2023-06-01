c58471a2753c077e4168d599143fdfb36a8bc766
PK <pk@Pranavs-MacBook-Pro.local>
2023-06-01 11:23:15 +0530

    signup code added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.json());
+
+
 const USERS = [];
 
 const QUESTIONS = [{
@@ -22,13 +25,29 @@ app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  const { email, password } = req.body;
+
+  const userExists = USERS.some(user => user.email === email);
+  if (userExists) {
+    return res.status(400).send('Email already exists');
+  }
+
+  USERS.push({ email, password });
+
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
 
   // return back 200 status code to the client
-  res.send('Hello World!')
+  return res.status(200).send('Signup successful');
+
+console.log(USERS);
+
+
+  
+
 })
+
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
