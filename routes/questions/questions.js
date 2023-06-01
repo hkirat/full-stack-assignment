a740f4abf86fb567699f9fbce5e415ef8571fc00
Swastik <swastikpanda32@gmail.com>
2023-06-02 01:39:40 +0530

    added logic to the questions route

--- routes/questions/questions.js
+++ routes/questions/questions.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const { USERS } = require("../../users");
 const router = express.Router();
+
+const QUESTIONS = [{
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+      input: "[1,2,3,4,5]",
+      output: "5"
+  }]
+}];
 
 router.get("/questions", function (req, res) {
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!");
+  const { title, description, testCases} = req.query;
+  const newQuestion = {title,description,testCases};
+  QUESTIONS.push(newQuestion)
+  res.sendStatus(200);
 });
 
 module.exports = router;
