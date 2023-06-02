28b93246ce70ef240936ffad98d93caac4a67a68
ayushanand308 <ayushanand_mc21a11_34@dtu.ac.in>
2023-06-02 15:28:50 +0530

    completed the leftover code and tested it on postman

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const bcrypt = require('bcryptjs');
+const jwt= require('jsonwebtoken');
+const secret = "dfanhfkjas435873249sjfk";
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const USERS = [];
+const USERS = [{
+  email: "your_email@example.com",
+  password: "your_password_here"
+}];
 
 const QUESTIONS = [{
     title: "Two states",
@@ -14,59 +23,70 @@ const QUESTIONS = [{
 }];
 
 
-const SUBMISSION = [
-
+const SUBMISSION = [{
+  question: "Two states",
+  code: "function max(arr) { return Math.max(...arr); }",
+  status: "Accepted"
+}
 ]
 
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  const {email, password} = req.body;
+  const userExists = USERS.find(user => user.email === email);
+  if(userExists) {
+    return res.status(400).send("User already exists");
+  }
 
+  bcrypt.hash(password, 12, function(err, hash) {
+    if(err) {
+      return res.status(500).send("Internal Server Error");
+    }
+    USERS.push({email, password: hash});
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+    res.status(200).send("User created successfully!");
+  })
 })
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  const {email, password} = req.body;
+  const user = USERS.find(user => user.email === email && user.password === password);
+  if(!user) {
+    return res.status(401).send("Invalid credentials");
+  }
+  else {
+    const token = jwt.sign({email}, secret, {expiresIn: "1h"});  
+    const response = {message:"Login successful", token};
+    return res.json(response);
+  }
 })
 
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  const questions = QUESTIONS.map(question => {
+    const title= question.title; 
+    const description = question.description; 
+    const testCases = question.testCases; 
+    return {title, description, testCases}}
+  );
+  res.status(200).send(questions);
 })
 
 app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
-});
-
+  const submissions = SUBMISSION.map(submission => {
+    const question = submission.question;
+    const code = submission.code;
+    const status = submission.status;
+    return {question, code, status};
+  })
+  res.status(200).send(submissions);
+})
 
 app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  const {question, code} = req.body;
+  const status = Math.random() > 0.5 ? "Accepted" : "Rejected";
+  SUBMISSION.push({question, code, status});
+  res.status(200).send("Submission created successfully");
 });
-
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
