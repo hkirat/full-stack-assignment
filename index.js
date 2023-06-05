33811c92ffb71f116911720483469e7ff9470d29
mani <111manikanta.v@gmail.com>
2023-06-05 15:03:12 +0530

    optimized assignment 1 completely

--- index.js
+++ index.js
@@ -19,44 +19,30 @@ const SUBMISSION = []
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-
-})
-
 app.post('/signup', function (req, res) {
   // Add logic to decode body
   // body should have email and password
+  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  // return back 200 status code to the client
 
-  let email = req.body.email;
-  let password = req.body.password;
-  let accType = req.body.position;
+  const { email, password, accType } = req.body;
+
   let user;
-  if (accType === "user") {
-    if (USERS.length != 0)
-      user = USERS.filter((user) => { return user.email === email });
 
-  } else {
+  if (accType === "user")
+    user = USERS.find(user => user.email === email);
+  else
+    user = ADMIN.find(user => user.email === email);
 
-    if (ADMIN.length != 0)
-      user = ADMIN.filter((user) => { return user.email === email });
-  }
+
   if (!user) {
-    console.log('user is added');
     accType === "user" ? USERS.push({ email, password }) : ADMIN.push({ email, password });
-
+    return res.status(200).send(`${accType} account is created for ${email}`);
   }
 
-  if (!user)
-    return res.status(200).send(`${accType} account is created for ${email}`);
-
-  console.log(user);
   return res.status(400).send("account already exists!");
 
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
 
 })
 
@@ -65,35 +51,26 @@ app.post('/login', function (req, res) {
   // body should have email and password
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-  let user;
-
-  if (USERS.length != 0)
-    user = USERS.filter((user) => { return user.email === req.body.email && user.password === req.body.password });
-
-  if (user)
-    res.status(200).cookie("autorizationId", "this is your key").send();
-
-  else res.status(401).send();
-
-
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
 
+  const user = USERS.find(user => user.email === req.body.email && user.password === req.body.password);
+
+  user ? res.cookie("autorizationId", "this is your key").status(200).send() : res.status(401).send();
+
 })
 
 app.get('/questions', function (req, res) {
-
   //return the user all the questions in the QUESTIONS array
   res.send(QUESTIONS);
 })
 
 app.get("/submissions", function (req, res) {
   // return the users submissions for this problem
-  const questionId = req.query.questionId;
-  const email = req.query.email;
+  const { title, email } = req.query;
 
-  const submissions = SUBMISSION.filter((record) => { return record.questionId === questionId && email === email });
+  const submissions = SUBMISSION.filter((record) => { return record.title === title && email === email });
 
   res.send(submissions);
 });
@@ -103,17 +80,14 @@ app.post("/submissions", function (req, res) {
   // let the user submit a problem, randomly accept or reject the solution
   // Store the submission in the SUBMISSION array above
 
-  let decision = Math.floor(Math.random() * 2);
-  const email = req.body.email;
-  const title = req.body.title;
-  const code = req.body.code;
+  const status = Math.floor(Math.random() * 2);
 
-  SUBMISSION.push({ email, title, code, status: decision })
+  const { email, title, code } = req.body;
 
-  if (decision)
-    res.send("solution accepted");
-  else
-    res.send("solutioin not accepted");
+  SUBMISSION.push({ email, title, code, status })
+
+  status ? res.send("solution accepted") : res.send("solutioin not accepted");
+
 });
 
 // leaving as hard todos
@@ -121,30 +95,16 @@ app.post("/submissions", function (req, res) {
 // ensure that only admins can do that.
 
 app.post('/addquestion', (req, res) => {
+  const { email, question, question: { title } } = req.body;
 
-  let email = req.body.email;
-  let question = req.body.question;
-  let questionId = req.body.question.title;
-  let admin;
-  if (ADMIN.length != 0)
-    admin = ADMIN.filter((user) => { return user.email === email });
+  const admin = ADMIN.find(user => user.email === email)
 
   if (!admin)
     return res.status(400).send("your not an admin");
 
-  let checkId;
-
-  if (QUESTIONS.length != 0)
-    checkId = QUESTIONS.filter((problem) => { return problem.title === questionId })
-
-  console.log(checkId);
-  if (checkId.length === 0)
-    QUESTIONS.push(question);
-  else
-    return res.status(401).send("question Id is already choosen");
+  QUESTIONS.find(problem => problem.title === title) ? res.status(401).send("question Id is already choosen") : QUESTIONS.push(question);
 
   return res.status(200).send("question added successfully");
-
 
 })
 
