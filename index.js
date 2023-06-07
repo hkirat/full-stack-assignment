7d28f7d80e2be36f2f6d0ed109b56f43254ca327
Karan Handa <karan.handa_ug23@ashoka.edu.in>
2023-06-07 23:56:01 +0530

    Partially completed the assignment. Done with login, signup and fetching questions.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const port = 3001;
+
+const app = express();
+app.use(bodyParser.json());
+
+/*
+USER FORMAT
+{
+  email: "...",
+  password: "...",
+  admin: 0/1 // 0 means regular user, 1 means admin
+}
+*/
 
 const USERS = [];
 
@@ -13,61 +26,179 @@ const QUESTIONS = [{
     }]
 }];
 
+/*
+SUBMISSIONS FORMAT
+{
+  email: "karan.handa.fox@gmail.com",
+  question_title: "NAME OF QUESTION",
+  language: "C++17",
+  code: "CODE"
+}
+*/
 
-const SUBMISSION = [
+const SUBMISSIONS = [];
 
-]
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    res.status(401).json({ message: 'Please Include an Authentication Token'} )
+  }
+
+  jwt.verify(token, 'secretKey', (err, user) => {
+    if (err) {
+      res.status(400);
+    }
+    req.user = user;
+    next();
+  })
+}
 
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
+  let password = req.body.password;
+  let email = req.body.email;
+
+  const user = USERS.find((user) => user.email === email);
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  if (user) {
+      // return back 401
+    res.status(401).json({ message: 'Email already exists' });
+  }
 
-
+  const newUser = {
+    email: email,
+    password: password,
+    admin: 0
+  };
+  USERS.push(newUser);
+  
   // return back 200 status code to the client
-  res.send('Hello World!')
+
+  res.status(200).json({ message : 'Signup Successful!' });
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  let password = req.body.password;
+  let email = req.body.email;
 
+  
+  // Check if the user with the given email exists in the USERS array
+  const user = USERS.find((user) => user.email === email);
+  
+  // Also ensure that the password is the same
+  if (!user || user.password != password) {
+    // If the password is not the same, return back 401 status code to the client
+    res.status(401).json( { message : 'Incorrect email or password'} );
+  }
 
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  const token = jwt.sign({ email: email }, 'secretKey');
 
+  res.setHeader('Authorization', `Bearer ${token}`);
 
-  res.send('Hello World from route 2!')
+  res.status(200).json({ message: 'Login Successful!'});
 })
 
-app.get('/questions', function(req, res) {
 
+app.get('/questions', authenticateToken, function(req, res) {
+
+  res.status(200);
+  res.send(QUESTIONS); 
+ 
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions", authenticateToken, function(req, res) {
+  // somehow get current user's name
+
+  // assert : user has been authenticated
+
+  const email = req.user.email;
+  const question_title = req.body.question_title;
+
+  const userSubmissionsList = SUBMISSIONS.filter((submission) => 
+    submission.question_title === question_title && submission.email === email
+  );
+
+  // return the users submissions for this problem
+  res.status(200).send(userSubmissionsList);
+
 });
 
 
-app.post("/submissions", function(req, res) {
+app.post("/submissions", authenticateToken, function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  
+  const question_title = req.body.question_title;
+  const email = req.user.email;
+  const language = req.body.language;
+  const code = req.body.code;
+
+  if (!isValid(code) || !isValid(language) || !isValid(question_title)) {
+    res.status(403).json({ message: 'Invalid Submission. Please make sure that code is non-empty, the language is valid, and the question exists'});
+  }
+
+  SUBMISSIONS.push({
+    email: email,
+    question_title: question_title,
+    language: language,
+    code: code
+  });
+   
+  res.status(200).json({ message: 'Submission Successful' })
+  // Store the submission in the SUBMISSIONS array above
 });
+
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
+function isAdmin(email) {
+  const user = USERS.find((user) => user.email === email);
+
+  if (user.admin === 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+app.post("/submitproblem", authenticateToken, (req, res) => {
+  
+  const email = req.user.email;
+
+  if (!isAdmin(email)) {
+    res.status(401).json({ message: 'You do not have admin priviledges' });
+  }
+      
+  const title = req.body.title;
+  const description = req.body.description;
+  const testCases = req.body.testCases;
+
+  if (!isValid(title) || !isValid(description) || !isValid(testCases)) {
+    res.status(403).json({ message: 'Please make sure the problem is correctly formatted'} );
+  }
+
+  QUESTIONS.push({
+      title: title,
+      description: description,
+      testCases: testCases
+  });
+
+  res.status(200).json({ message: 'Question successfully added' });
+});
+
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
