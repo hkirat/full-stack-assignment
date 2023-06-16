5a76d70b9de218564fc2951dba32e31916f8c832
kirans0325 <kirans0325@gmail.com>
2023-06-16 07:53:46 +0530

    solved assignment

--- index.js
+++ index.js
@@ -1,73 +1,104 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
 const USERS = [];
-
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    description: "Given an array, return the maximum of the array?",
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
         output: "5"
-    }]
-}];
+      }
+    ]
+  }
+];
+const SUBMISSIONS = [];
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const SUBMISSION = [
+//Starting Page//
+app.get('/', function(req, res) {
+  res.send("Welcome to Starting page");
+});
 
-]
-
+// Routes
 app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  const { email, password } = req.body;
 
+  // Check if a user with the given email already exists
+  const userExists = USERS.some(user => user.email === email);
+  if (userExists) {
+    return res.status(400).send('User already exists');
+  }
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  // Store email and password in the USERS array
+  USERS.push({ email, password });
 
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
+  // Return a success response
+  return res.status(200).send('User created successfully');
+});
 
 app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  const { email, password } = req.body;
 
   // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const user = USERS.find(user => user.email === email);
 
+  if (user && user.password === password) {
+    // Return a success response and a token
+    return res.status(200).send('Login successful. Token: YOUR_TOKEN_HERE');
+  } else {
+    // Return an error response
+    return res.status(401).send('Incorrect email or password');
+  }
+});
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
+//Questions Page//
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  res.json(QUESTIONS);
+});
+//Submissions Page//
+app.get('/submissions', function(req, res) {
+  res.json(SUBMISSIONS);
 });
 
+app.post('/submissions', function(req, res) {
+  const submission = req.body;
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  // Randomly accept or reject the submission
+  const isAccepted = Math.random() < 0.5;
+
+  submission.status = isAccepted ? 'Accepted' : 'Rejected';
+
+  // Store the submission in the SUBMISSIONS array
+  SUBMISSIONS.push(submission);
+
+  // Return a success response
+  return res.status(200).send('Submission created successfully');
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+// Admin-only route to add a new problem
+app.post('/admin/add-problem', function(req, res) {
+  
+  const isAdmin = true; 
+  if (!isAdmin) {
+    return res.status(403).send('Access denied');
+  }
+
+  const newProblem = req.body;
+
+  // Add the new problem to the QUESTIONS array
+  QUESTIONS.push(newProblem);
+
+  // Return a success response
+  return res.status(200).send('Problem added successfully');
+});
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
