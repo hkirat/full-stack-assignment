04908c8ea3a3d37daf1da10fa40424fe12a3ea1b
Sarthak <srnegi141@gmail.com>
2023-06-18 13:59:10 -0500

    Complete

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const app = express()
 const port = 3000
 app.use(express.json());
 
-var userIndex = 0;
-
 const USERS = [];
-const SUBMISSION = [];
+const SUBMISSIONS = [];
 
 const QUESTIONS = [{
     title: "Two states",
@@ -61,27 +59,26 @@ app.post('/login', function(req, res) {
   }
 });
 
-// app.get('/questions', function(req, res) {
 
-//   //return the user all the questions in the QUESTIONS array
-//   res.send("Hello World from route 3!")
-// })
+app.get('/questions', function(req, res) {
+  res.send(QUESTIONS);
+})
 
-// app.get("/submissions", function(req, res) {
-//    // return the users submissions for this problem
-//   res.send("Hello World from route 4!")
-// });
+app.get("/submissions", function(req, res) {
+  res.send(SUBMISSIONS);
+});
 
 
-// app.post("/submissions", function(req, res) {
-//    // let the user submit a problem, randomly accept or reject the solution
-//    // Store the submission in the SUBMISSION array above
-//   res.send("Hello World from route 4!")
-// });
+app.post("/submissions", function(req, res) {
+  const { question, code } = req.body;
+  const submissionStatus = Math.random() > 0.5 ? "Accepted": "Rejected";
+  SUBMISSIONS.push({question,code,submissionStatus});
+  res.send("Submission Successful!");
+});
 
-// // leaving as hard todos
-// // Create a route that lets an admin add a new problem
-// // ensure that only admins can do that.
+app.post("/problems", function(req,res){
+
+});
 
 app.listen(port, function() {
   console.log(`Example app listening at http://localhost:${port}`)
