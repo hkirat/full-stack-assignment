4a63668e6156728e17d6cdd6ae2b9f027ef71ed6
shannu0003 <129636365+shannu0003@users.noreply.github.com>
2023-06-21 14:56:00 +0530

    final draft

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3001;
+const express = require('express')
+const app = express()
+const port = 3001
 
-const USERS = [];
+const USERS = [
+  
+]
 
 const QUESTIONS = [{
   title: "whole multiplication",
@@ -13,16 +15,20 @@ const QUESTIONS = [{
   }]
 }];
 
-const SUBMISSIONS = [];
+const SUBMISSIONS = [
+
+
+  
+]
 
 // Route for user signup
 app.post('/signup', function (req, res) {
   const { email, password } = req.body;
 
   // Check if user already exists
-  const userExists = USERS.some(user => user.email === email);
+  const userExists = USERS.some(user => user.email === email)
   if (userExists) {
-    return res.status(400).send('User already exists');
+    return res.status(400).send('User already exists')
   }
 
   // Create new user
@@ -30,9 +36,9 @@ app.post('/signup', function (req, res) {
     email,
     password
   };
-  USERS.push(newUser);
+  USERS.push(newUser)
 
-  res.sendStatus(200);
+  res.sendStatus(200)
 });
 
 // Route for user login
@@ -40,18 +46,18 @@ app.post('/login', function (req, res) {
   const { email, password } = req.body;
 
   // Checking if user exists and password matches
-  const user = USERS.find(user => user.email === email);
+  const user = USERS.find(user => user.email === email)
   if (!user || user.password !== password) {
-    return res.status(401).send('Invalid email or password');
+    return res.status(401).send('Invalid email or password')
   }
 
   // Return success response with token
-  res.status(200).json({ token: 'randomToken' });
+  res.status(200).json({ token: 'randomToken' })
 });
 
 // created Route for retrieving questions
 app.get('/questions', function (req, res) {
-  res.json(QUESTIONS);
+  res.json(QUESTIONS)
 });
 
 // Route for retrieving user submissions for a problem
@@ -59,14 +65,14 @@ app.get('/submissions', function (req, res) {
   const { userId } = req.query;
 
   // Filter submissions based on userId
-  const userSubmissions = SUBMISSIONS.filter(submission => submission.userId === userId);
+  const userSubmissions = SUBMISSIONS.filter(submission => submission.userId === userId)
   
-  res.json(userSubmissions);
+  res.json(userSubmissions)
 });
 
 // Route for submitting a problem solution
 app.post('/submissions', function (req, res) {
-  const { userId, problemId, solution } = req.body;
+  const { userId, problemId, solution } = req.body
 
   // Randomly accept or reject the solution
   const isAccepted = Math.random() < 0.5;
@@ -79,9 +85,9 @@ app.post('/submissions', function (req, res) {
     isAccepted
   };
 
-  SUBMISSIONS.push(newSubmission);
+  SUBMISSIONS.push(newSubmission)
 
-  res.sendStatus(200);
+  res.sendStatus(200)
 });
 
 // Route for adding a new problem (Only accessible by admins)
@@ -90,15 +96,15 @@ app.post('/problems', function (req, res) {
 
   // Check if user is an admin
   if (!isAdmin) {
-    return res.status(403).send('Only admins can add new problems');
+    return res.status(403).send('Only admins can add new problems')
   }
 
   // Add the new problem to QUESTIONS array
-  QUESTIONS.push(problem);
+  QUESTIONS.push(problem)
 
-  res.sendStatus(200);
+  res.sendStatus(200)
 });
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`)
 });
