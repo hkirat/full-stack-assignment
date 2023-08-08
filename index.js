b8775d3beee43da760a415afc7585378f633ae34
Md. Abu Taief Siddique <103975114+TaiefSiddique@users.noreply.github.com>
2023-08-09 03:51:39 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,73 +1,78 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const USERS = [];
 
-const QUESTIONS = [{
+const QUESTIONS = [
+  {
     title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
+    description: "Given an array, return the maximum of the array?",
+    testCases: [
+      {
         input: "[1,2,3,4,5]",
         output: "5"
-    }]
-}];
+      }
+    ]
+  }
+];
 
+const SUBMISSIONS = [];
 
-const SUBMISSION = [
+app.use(express.json()); // Middleware to parse JSON request bodies
 
-]
+// Signup route
+app.post('/signup', function (req, res) {
+  const { email, password } = req.body; // Assuming the request body contains email and password
+  
+  // Check if a user with the given email already exists
+  const existingUser = USERS.find(user => user.email === email);
+  if (existingUser) {
+    return res.status(400).send("User already exists");
+  }
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+  // Add the new user to the USERS array
+  USERS.push({ email, password });
 
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
-})
-
-app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
-
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  return res.status(200).send("User registered successfully");
 });
 
+// Login route
+app.post('/login', function (req, res) {
+  const { email, password } = req.body; // Assuming the request body contains email and password
+  
+  // Find the user with the given email
+  const user = USERS.find(user => user.email === email);
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  if (!user || user.password !== password) {
+    return res.status(401).send("Invalid email or password");
+  }
+
+  // Generate a token (for simplicity, using a random string)
+  const token = Math.random().toString(36).substr(2);
+  
+  return res.status(200).json({ token }); // Return token as JSON response
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
+// Questions route
+app.get('/questions', function (req, res) {
+  return res.status(200).json(QUESTIONS); // Return QUESTIONS array as JSON response
+});
+
+// Submissions route
+app.post('/submissions', function (req, res) {
+  const submission = req.body; // Assuming the request body contains the submission object
+  
+  // Randomly accept or reject the submission (for simplicity)
+  const isAccepted = Math.random() < 0.5;
+  submission.status = isAccepted ? "Accepted" : "Rejected";
+  
+  // Store the submission in the SUBMISSIONS array
+  SUBMISSIONS.push(submission);
+
+  return res.status(200).json(submission); // Return submission status as JSON response
+});
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
