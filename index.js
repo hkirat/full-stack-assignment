5c170ad2560a1025defc7e03dd0fc26e161f413d
Wackyator <pratapsinghtarun9@gmail.com>
2023-08-12 18:07:08 +0530

    remove stuff

--- index.js
+++ index.js
@@ -209,10 +209,6 @@ app.post("/submissions", function (req, res) {
   return;
 });
 
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
-
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
