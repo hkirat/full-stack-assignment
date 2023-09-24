3f9e7b272bc484028b7b7b2b8f230e441132ff38
vaibhavi tyagi <vaibhavityagi5@gmail.com>
2023-09-24 19:02:12 +0530

    added login and signup routes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const { access } = require("fs");
 
 const app = express();
 const port = 3000;
+
+const ADMIN = { email: "janedoe@admin", password: "000000" };
 
 const USERS = [];
 
@@ -26,6 +29,23 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+// middleware
+const isAdmin = (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === ADMIN.email && password === ADMIN.password) {
+    return next();
+  }
+  res.send("You dont have access to this route");
+};
+
+app.get("/signup", (req, res) => {
+  res.render("users/signup");
+});
+
+app.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
 app.post("/signup", function (req, res) {
   // Add logic to decode body
   // body should have email and password
@@ -33,11 +53,21 @@ app.post("/signup", function (req, res) {
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
 
   // return back 200 status code to the client
-  res.send("Hello World!");
+
+  const { email, password } = req.body;
+
+  for (let user of USERS) {
+    if (user.email === email) {
+      console.log("email already exists");
+      return res.redirect("/login");
+    }
+  }
+  USERS.push(req.body);
+  res.status(200).redirect("/questions");
 });
 
 app.post("/login", function (req, res) {
-  // Add logic to decode body
+  // Add logic to decode body  - DONE
   // body should have email and password
 
   // Check if the user with the given email exists in the USERS array
@@ -47,29 +77,47 @@ app.post("/login", function (req, res) {
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
 
-  res.send("Hello World from route 2!");
+  const { email, password } = req.body;
+
+  for (let user of USERS) {
+    if (user.email === email && user.password === password) {
+      return res.status(200).send("Congratulations, you are logged in!");
+    }
+  }
+  res.send(401);
 });
 
 app.get("/questions", function (req, res) {
-  //return the user all the questions in the QUESTIONS array - DONE
+  //return the user all the questions in the QUESTIONS array
   res.render("questions", { questions: QUESTIONS });
 });
 
 app.get("/submissions", function (req, res) {
-  // return the users submissions for this problem - DONE
+  // return the users submissions for this problem
   res.render("submissions", { submissions: SUBMISSION });
 });
 
 app.post("/submissions", function (req, res) {
-  // let the user submit a problem, randomly accept or reject the solution - DONE
+  // let the user submit a problem, randomly accept or reject the solution
   // Store the submission in the SUBMISSION array above
-  SUBMISSION.push(req.body.submission);
-  res.redirect("/submissions");
+
+  const accepted = () => Math.random();
+
+  if (accepted()) {
+    SUBMISSION.push(req.body.submission);
+    return res.redirect("/submissions");
+  }
+  res.redirect("/questions");
 });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
+
+// at the moment this will only work if request to the route is send via postman with body
+app.get("/newQuestions", isAdmin, (req, res) => {
+  res.send("Add new questions here");
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
