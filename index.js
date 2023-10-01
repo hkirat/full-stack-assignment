d95cd6b8eff1117c79fe49d69c6b3378e096514f
Chahbaz Aman <chahbaza@gmail.com>
2023-10-02 02:58:42 +0530

    Implemented /login path function

--- index.js
+++ index.js
@@ -1,78 +1,97 @@
 const express = require('express')
-const fs  = require('fs');
 const app = express()
 const port = 3001
 
-const USERS = [];
+app.set('view engine', 'ejs')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.redirectWithStatus = function(statusCode, path) {
+    this.status(statusCode);
+    this.redirect(path);
+  };
+  next();
+})
+
+var USERS = {};
 
 const QUESTIONS = [{
-    title: "Two states",
+    title: "Max of array",
     description: "Given an array , return the maximum of the array?",
     testCases: [{
         input: "[1,2,3,4,5]",
         output: "5"
     }]
-}];
+  },
+  {
+    title: "Sorting",
+    description: "Given an array , return the sorted array?",
+    testCases: [{
+        input: "[4,2,1,3,5]",
+        output: "[1,2,3,4,5]"
+    }]
+  }
+];
 
 
 const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+app.post('/signup', function(req, res, next) {
+  console.log(`Registering Username: ${req.body.email}, Password: ${req.body.password}`);
+  USERS[req.body.email] = req.body.password
+  next()
+  }, function(req, res) {
+  res.status(200).redirect('/?message=User+registration+complete.+Please+try+logging+in');
 })
 
-app.post('/login', function(req, res) {
+app.post('/login', function(req, res, next) {
   // Add logic to decode body
   // body should have email and password
+  const email = req.body.email
+  const password = req.body.password
 
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
-
+  if (email in USERS) {
+    if (USERS[email] == password) {
+      next()
+    }
+    else  {
+      res.status(401).send()
+    }
+  }
+  else {
+    res.status(401).send()
+  }
+  }, function (req, res) {
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
+  res.json({token: 'the token is 42'}).status(200)
 })
 
 app.get('/', (req, res) => {
-    fs.readFile('templates/home.html', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file', err);
-            return res.status(500).send('Internal Server Error');
-        }
-        res.send(data);
-    });
+  res.render('home', {message: req.query.message});
 });
 
 app.get('/questions', function(req, res) {
-
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.send(QUESTIONS)
 })
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+  res.send(SUBMISSION)
 });
 
 
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+  res.send("Submitting a Solution is under construction")
 });
 
 // leaving as hard todos
