b45490af8e372199a54537bd750ef691ccb43f58
maruf-rahman007 <s.marufrahman.g@gmail.com>
2023-11-02 01:27:21 +0600

    Full stack Assignment 1 completed

--- index.js
+++ index.js
@@ -1,73 +1,138 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
-const USERS = [];
+const USERS = [
+  { email: 'dummyuser1@gmail.com', password: 'password1' },
+  { email: 'dummyuser2@gmail.com', password: 'password2' },
+];
 
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'djvgnJVBLKndbgWLKDGKwdbgnhWGKNQGLKwrnghkWNGKwrgn';
 
+const QUESTIONS = [
+  {
+    title: 'Two states',
+    description: 'Given an array, return the maximum of the array?',
+    testCases: [
+      {
+        input: '[1,2,3,4,5]',
+        output: '5',
+      },
+    ],
+  },
+];
 
-const SUBMISSION = [
+const SUBMISSIONS = [];
 
-]
+const ADMIN_TOKEN = 'tyhurtyioerhgjbgnbnfjgbndjhgj';
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
-
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+const HARD_TODOS = [];
 
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+app.use(express.json());
 
+// function to generate a Web token for users
+function generateUserToken(user) {
+  return jwt.sign({ email: user.email }, jwtSecret, { expiresIn: '1h' });
+}
 
-  res.send('Hello World from route 2!')
-})
+// Signup route
+app.post('/signup', function (req, res) {
+  const { email, password } = req.body;
+  const existingUser = USERS.find((user) => user.email === email);
 
-app.get('/questions', function(req, res) {
+  if (existingUser) {
+    res.status(409).send('User with this email already exists');
+  } else {
+    USERS.push({ email, password });
 
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+    // Generate a web token for the newly signed up user
+    const token = generateUserToken({ email });
+    res.status(200).json({ message: 'User signed up successfully', token });
+  }
+});
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+// Login route
+app.post('/login', function (req, res) {
+  const { email, password } = req.body;
+  const user = USERS.find((user) => user.email === email);
+
+  if (!user || user.password !== password) {
+    res.status(401).send('Authentication failed');
+  } else {
+    // Generate a web token for the logged-in user
+    const token = generateUserToken(user);
+    res.status(200).json({ message: 'Authentication successful', token });
+  }
+});
+
+// Questions route
+app.get('/questions', function (req, res) {
+  res.status(200).json(QUESTIONS);
+});
+
+// Submissions route
+app.post('/submissions', function (req, res) {
+  const { problem, solution } = req.body;
+
+  if (!problem || !solution) {
+    res.status(400).send('Missing problem or solution in the request body');
+  } else {
+    const isAccepted = Math.random() < 0.5;
+    SUBMISSIONS.push({ problem, solution, isAccepted });
+
+    if (isAccepted) {
+      res.status(200).json({ message: 'Submission accepted' });
+    } else {
+      res.status(200).json({ message: 'Submission rejected' });
+    }
+  }
+});
+
+// Admin route to add a new problem
+app.post('/admin/addproblem', function (req, res) {
+  const adminToken = req.header('Authorization');
+
+  if (adminToken !== ADMIN_TOKEN) {
+    res.status(401).send('Unauthorized');
+  } else {
+    const { title, description, testCases } = req.body;
+
+    if (!title || !description || !testCases) {
+      res.status(400).send('Missing required fields for the problem');
+    } else {
+      QUESTIONS.push({ title, description, testCases });
+      res.status(200).send('Problem added successfully');
+    }
+  }
+});
+
+// Hard TO-DOs
+
+app.post('/user/addhardtodo', authenticateUser, (req, res) => {
+  const user = req.user; // Assuming you have user information in the request
+  const { problem } = req.body;
+
+  if (!problem) {
+    res.status(400).send('Missing problem information');
+  } else {
+    // Find the user's entry in HARD_TODOS or create one if it doesn't exist
+    let userEntry = HARD_TODOS.find(entry => entry.user === user.email);
+
+    if (!userEntry) {
+      userEntry = { user: user.email, hardTodos: [] };
+      HARD_TODOS.push(userEntry);
+    }
+
+    // Add the problem to the user's hard todo list
+    userEntry.hardTodos.push(problem);
+
+    res.status(200).send('Problem added to hard todo list');
+  }
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
 });
-
-// leaving as hard todos
-// Create a route that lets an admin add a new problem
-// ensure that only admins can do that.
-
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
-})
