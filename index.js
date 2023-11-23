5af7539d419fc9583399f0e89868ea87795260b4
Ranjit Yadav <yadavranjit521@gmail.com>
2023-11-23 13:09:35 +0530

    added sign-up and login functionality

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// imports
 const express = require('express')
+const bcryptjs = require('bcryptjs')
+
+// inits
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const port = 3001
 
 const USERS = [];
@@ -13,37 +20,69 @@ const QUESTIONS = [{
     }]
 }];
 
-
 const SUBMISSION = [
 
 ]
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+// routes
+app.post('/signup', async function(req, res) {
+  const { name, email, password } = req.body
 
+  if (!email || !password) {
+    return res.status(400).json({
+      message: 'Email and password are required'
+    })
+  }
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  if (USERS.find(user => user.email === email)) {
+    return res.status(400).json({
+      message: 'User with given email already exists'
+    })
+  }
 
+  const hashedPassword = await bcryptjs.hash(password, 10)
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
+  const newUser = {
+    name,
+    email,
+    password: hashedPassword
+  }
+  USERS.push(newUser)
+
+  res.status(200).json({
+    message: 'User created successfully!',
+    user: newUser,
+  })
 })
 
-app.post('/login', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+app.post('/login', async function(req, res) {
+  const { email, password } = req.body
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
+  const user = USERS.find(user => user.email === email)
+  if (!user) {
+    return res.status(401).json({
+      message: 'Invalid credentials!',
+    })
+  }
 
+  const isPasswordValid = await bcryptjs.compare(password, user.password)
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: 'Invalid credentials!',
+    })
+  }
 
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
+  const token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  const updatedUser = {
+    name: user.name,
+    email: user.email,
+    token: token,
+  }
 
-
-  res.send('Hello World from route 2!')
+  res.json({
+    message: 'Login successful!',
+    user: updatedUser,
+  })
 })
 
 app.get('/questions', function(req, res) {
