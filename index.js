959e719ff9e0e8e27fe5f7f114d434f979b09403
shashwat558 <tumbaktuhere@gmail.com>
2023-12-03 12:39:18 +0530

    Added the signup logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const parser = require('body-parser')
+const port = 3000
 
 const USERS = [];
 
@@ -18,22 +19,29 @@ const SUBMISSION = [
 
 ]
 
+app.use(express.json())
+
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
-
-
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
   // return back 200 status code to the client
-  res.send('Hello World!')
+  const {email, password} = req.body;
+  const user = USERS.find(u => u.email === email && u.password === password);
+  if(!user) {
+     newUser = {email, password};
+     USERS.push(newUser)
+     return res.status(200).send("registered")
+  } else {
+    return res.status(400).send('Email already exist')
+  }
+``
+  
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
-
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
 
